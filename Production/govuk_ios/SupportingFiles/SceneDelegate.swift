03622622dd5e8a92c0404b8437b6fd69bc20ab39
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-04-24 12:17:20 +0100

    Removed commented code

--- Production/govuk_ios/SupportingFiles/SceneDelegate.swift
+++ Production/govuk_ios/SupportingFiles/SceneDelegate.swift
@@ -4,6 +4,7 @@
 // Copyright © 2024 Government Digital Services.
 // All Rights Reserved.
 //
+
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -16,7 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-//        guard let _ = (scene as? UIWindowScene) else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

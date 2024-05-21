eba1f86916921d45c114eaf2320da85b64d27633
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-05-21 16:00:41 +0100

    Added TestAppDelegate and TestSceneDelegate

--- Tests/govuk_ios/shared/SupportingFiles/TestAppDelegate.swift
+++ Tests/govuk_ios/shared/SupportingFiles/TestAppDelegate.swift
@@ -1,16 +1,14 @@
 import UIKit
 
-class AppDelegate: UIResponder,
-                   UIApplicationDelegate {
+class TestAppDelegate: UIResponder, 
+                       UIApplicationDelegate {
     func application(_ application: UIApplication,
                      configurationForConnecting connectingSceneSession: UISceneSession,
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        let sceneConfiguration = UISceneConfiguration(
-            name: "Default Configuration",
-            sessionRole: connectingSceneSession.role
-        )
-        sceneConfiguration.delegateClass = SceneDelegate.self
+        let sceneConfiguration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        sceneConfiguration.delegateClass = TestSceneDelegate.self
         sceneConfiguration.storyboard = nil
+
         return sceneConfiguration
     }
 }

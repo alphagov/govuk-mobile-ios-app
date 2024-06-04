3d831654dc41c5b6024a9ce3b8aa97e3ddf82af2
raphael olumofe <raphaelo@hotmail.co.uk>
2024-06-04 10:00:40 +0100

    task: this commit adds back a navigationController to the Deeplink protocol

--- Production/govuk_ios/DeepLinking/DeepLink.swift
+++ Production/govuk_ios/DeepLinking/DeepLink.swift
@@ -5,6 +5,7 @@ import UIKit
 
 protocol DeepLink {
     var path: String { get set }
+    var navigationController:UINavigationController? { get set }
     var isAvailable:Bool { get set }
     func displayUnavailableAlert()
     func action()

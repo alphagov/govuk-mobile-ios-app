97a52615c85c38b7b3c6e7118cd60fa1831d261f
Robin Phillips <rlphillips@deloitte.co.uk>
2024-06-21 12:52:34 +0100

    resolving code smells

--- Production/govuk_ios/UIViewComponents/PrimaryButton.swift
+++ Production/govuk_ios/UIViewComponents/PrimaryButton.swift
@@ -19,6 +19,17 @@ struct ButtonWrapper<WrappedButton: WrappableButton>: UIViewRepresentable {
     let title: String
     let icon: String?
 
+    init(title: String,
+         icon: String? = nil,
+         action:
+         @escaping () -> Void) {
+        self.title = title
+        self.icon = icon
+        self.action = UIAction { _ in
+            action()
+        }
+    }
+
     func makeUIView(context: Context) -> WrappedButton {
         let button = WrappedButton(action: action)
 
@@ -31,18 +42,11 @@ struct ButtonWrapper<WrappedButton: WrappableButton>: UIViewRepresentable {
         return button
     }
 
-    func updateUIView(_ uiView: WrappedButton, context: Context) { }
+    func updateUIView(_ uiView: WrappedButton, context: Context) {
+        // required for protocol conformance
+    }
 
-    func makeCoordinator() -> Self.Coordinator { }
-
-    init(title: String,
-         icon: String? = nil,
-         action:
-         @escaping () -> Void) {
-        self.title = title
-        self.icon = icon
-        self.action = UIAction { _ in
-            action()
-        }
+    func makeCoordinator() -> Self.Coordinator {
+        // required for protocol conformance
     }
 }

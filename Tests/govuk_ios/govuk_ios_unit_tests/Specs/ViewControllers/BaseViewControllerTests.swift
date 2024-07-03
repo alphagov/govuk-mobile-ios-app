63436660f9d31ca9c6da4932141733cda9081a73
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-07-03 15:15:39 +0100

    Added unit test for BaseViewController layoutMargins

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/ViewControllers/BaseViewControllerTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/ViewControllers/BaseViewControllerTests.swift
@@ -11,4 +11,10 @@ class BaseViewControllerTests: XCTestCase {
         XCTAssertEqual(subject.preferredStatusBarStyle, .lightContent)
     }
 
+    func test_layoutMargins_returnsExpectedValue() {
+        let subject = BaseViewController()
+        let expectedInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        XCTAssertEqual(subject.view.layoutMargins, expectedInsets)
+    }
+
 }

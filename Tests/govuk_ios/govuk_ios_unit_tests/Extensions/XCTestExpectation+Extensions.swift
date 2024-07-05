1f9af09b0e14ec02189f02baddf942e812717788
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-07-05 16:33:00 +0100

    Removed spacing in XCTestExpectation+Extensions

--- Tests/govuk_ios/govuk_ios_unit_tests/Extensions/XCTestExpectation+Extensions.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Extensions/XCTestExpectation+Extensions.swift
@@ -2,7 +2,6 @@ import Foundation
 import XCTest
 
 extension XCTestExpectation {
-
     func fulfillAfter(_ time: TimeInterval) {
         DispatchQueue.main.asyncAfter(deadline: .now() + time) {
             self.fulfill()

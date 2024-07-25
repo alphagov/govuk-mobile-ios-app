fdd371ba768da12b2e540a13af624720f69cc4e1
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-07-25 16:23:42 +0100

    Added OnboardingServiceTests

---
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Services/OnboardingServiceTests.swift
@@ -0,0 +1,39 @@
+import UIKit
+import Foundation
+import XCTest
+
+@testable import govuk_ios
+
+final class OnboardingServiceTests: XCTestCase {
+    func test_hasSeenOnboarding_hasSeen_returnsTrue() {
+        let userDefaults = UserDefaults()
+        userDefaults.set(bool: true, forkey: .appOnboardingSeen)
+        let subject = OnboardingService(userDefaults: userDefaults)
+
+        XCTAssertTrue(subject.hasSeenOnboarding)
+    }
+
+    func test_hasSeenOnboarding_hasntSeen_returnsTrue() {
+        let userDefaults = UserDefaults()
+        userDefaults.set(bool: false, forkey: .appOnboardingSeen)
+        let subject = OnboardingService(userDefaults: userDefaults)
+
+        XCTAssertFalse(subject.hasSeenOnboarding)
+    }
+
+    func test_setHasSeenOnboarding_setsTrue() {
+        let userDefaults = UserDefaults()
+        let subject = OnboardingService(userDefaults: userDefaults)
+        subject.setHasSeenOnboarding()
+
+        XCTAssertTrue(userDefaults.bool(forKey: .appOnboardingSeen))
+    }
+
+    func test_fetchSlides_returnsExpectedResult() {
+        let subject = OnboardingService(userDefaults: .init())
+        let result = subject.fetchSlides()
+
+        XCTAssertEqual(result.count, 3)
+    }
+
+}

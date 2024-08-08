1488ceed1a902bfdae8b2a1dbbe30d8337d2a279
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-08-08 14:24:15 +0100

    Added more BaseViewControllerTests

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/Models/HomeViewModelTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Models/HomeViewModelTests.swift
@@ -4,7 +4,6 @@ import XCTest
 @testable import govuk_ios
 
 class HomeViewModelTests: XCTestCase {
-    @MainActor
     func test_widgets_returnsArrayOfWidgets() {
         let subject = HomeViewModel()
         let widgets = subject.widgets

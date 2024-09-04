29655f5ff2bd07931f975b4e39b25c3c43b45c7d
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2024-09-04 15:16:12 +0100

    Refactor SearchViewController
    - use UISearchBar over custom textfield
    - enables empty search
    - styling fixes
    - removes supersuper modal view container

--- Production/govuk_ios/ViewControllers/SearchViewController.swift
+++ Production/govuk_ios/ViewControllers/SearchViewController.swift
@@ -5,61 +5,49 @@ class SearchViewController: BaseViewController,
                             TrackableScreen {
     private let viewModel: SearchViewModel
 
-    private lazy var modalView: UIView = {
-        let view = UIView()
-        view.backgroundColor = GOVUKColors.fills.surfaceModal
-        view.translatesAutoresizingMaskIntoConstraints = false
-//        view.clipsToBounds = true
-        return view
-    }()
-
     private lazy var searchBoxView: UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = GOVUKColors.fills.surfaceSearchBox
-        view.layer.cornerRadius = 10
-        return view
+        let localView = UIView()
+
+        localView.translatesAutoresizingMaskIntoConstraints = false
+        localView.backgroundColor = GOVUKColors.fills.surfaceSearchBox
+        localView.layer.cornerRadius = 10
+
+        return localView
     }()
 
-    private lazy var searchBoxTextField: UITextField = {
-        let textField = UITextField()
-        let searchIconPadding = 8
-        let imageView = UIImageView(frame: CGRect(
-            x: searchIconPadding, y: 0, width: 25, height: 22)
-        )
-        let imageContainerView = UIView(
-            frame: CGRect(
-                x: 0, y: 0, width: Int(imageView.frame.width) + (searchIconPadding * 2), height: 22
-            )
-        )
-        let image = UIImage(systemName: "magnifyingglass")
+    private lazy var searchBar: UISearchBar = {
+        let localSearchBar = UISearchBar()
 
-        imageContainerView.addSubview(imageView)
+        let clearButton = localSearchBar
+            .searchTextField
+            .value(forKey: "clearButton") as? UIButton
+        let image = clearButton?
+            .currentImage?
+            .withTintColor(UIColor.govUK.text.trailingIcon)
+        clearButton?.setImage(image, for: .normal)
 
-        imageView.image = image
-        imageView.contentMode = .center
-        imageView.tintColor = GOVUKColors.text.secondary
-
-        textField.accessibilityLabel = "Text field"
-        textField.accessibilityHint = "Returns content on GOV.UK"
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.attributedPlaceholder = NSAttributedString(
+        localSearchBar.enablesReturnKeyAutomatically = false
+        localSearchBar.translatesAutoresizingMaskIntoConstraints = false
+        localSearchBar.placeholder = "Search"
+        localSearchBar.backgroundImage = UIImage()
+        localSearchBar.searchTextField.attributedPlaceholder = NSAttributedString(
             string: "Search",
-            attributes: [NSAttributedString.Key.foregroundColor: GOVUKColors.text.secondary]
+            attributes: [
+                NSAttributedString.Key.foregroundColor: UIColor.govUK.text.secondary,
+                NSAttributedString.Key.font: UIFont.govUK.body
+            ]
         )
-        textField.textColor = GOVUKColors.text.primary
-        textField.clearButtonMode = .always
-        textField.leftViewMode = .always
-        textField.leftView = imageContainerView
-        textField.addTarget(
+        localSearchBar.searchTextField.leftView?.tintColor = UIColor.govUK.text.secondary
+        localSearchBar.searchTextField.addTarget(
             self,
-            action: #selector(onSearchReturn),
+            action: #selector(searchReturn),
             for: UIControl.Event.editingDidEndOnExit
         )
-        return textField
+
+        return localSearchBar
     }()
 
-    var trackingName: String { "searchmodal" }
+    var trackingName: String { "Search_Modal" }
 
     init(viewModel: SearchViewModel) {
         self.viewModel = viewModel
@@ -85,41 +73,30 @@ class SearchViewController: BaseViewController,
     public override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        searchBoxTextField.becomeFirstResponder()
+        searchBar.becomeFirstResponder()
     }
 
     private func configureUI() {
+        view.backgroundColor = GOVUKColors.fills.surfaceModal
         configureNavBar()
-        view.addSubview(modalView)
-        modalView.addSubview(searchBoxView)
-        searchBoxView.addSubview(searchBoxTextField)
+        view.addSubview(searchBar)
     }
 
     private func configureConstraints() {
         NSLayoutConstraint.activate([
-//            modalView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            modalView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            modalView.topAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.topAnchor
+            searchBar.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor,
+                constant: -10
             ),
-            modalView.leftAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.leftAnchor
+            searchBar.leftAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leftAnchor,
+                constant: 10
             ),
-            modalView.rightAnchor.constraint(
-                equalTo: view.safeAreaLayoutGuide.rightAnchor
+            searchBar.rightAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.rightAnchor,
+                constant: -10
             ),
-            modalView.heightAnchor.constraint(equalToConstant: view.bounds.height),
-
-            searchBoxView.topAnchor.constraint(
-                equalTo: modalView.topAnchor),
-            searchBoxView.leftAnchor.constraint(equalTo: modalView.leftAnchor, constant: 16),
-            searchBoxView.rightAnchor.constraint(equalTo: modalView.rightAnchor, constant: -16),
-            searchBoxView.heightAnchor.constraint(equalToConstant: 36),
-
-            searchBoxTextField.topAnchor.constraint(equalTo: searchBoxView.topAnchor),
-            searchBoxTextField.leftAnchor.constraint(equalTo: searchBoxView.leftAnchor),
-            searchBoxTextField.rightAnchor.constraint(equalTo: searchBoxView.rightAnchor),
-            searchBoxTextField.heightAnchor.constraint(equalTo: searchBoxView.heightAnchor)
+            searchBar.heightAnchor.constraint(greaterThanOrEqualToConstant: 36)
         ])
     }
 
@@ -130,7 +107,6 @@ class SearchViewController: BaseViewController,
         appearence.titleTextAttributes = [
             NSAttributedString.Key.font: UIFont.govUK.bodySemibold
         ]
-//        appearence.titlePositionAdjustment.vertical = 8
         appearence.configureWithTransparentBackground()
         appearence.backgroundColor = GOVUKColors.fills.surfaceModal
 
@@ -139,12 +115,6 @@ class SearchViewController: BaseViewController,
         )
         let attributes = [NSAttributedString.Key.foregroundColor: GOVUKColors.text.link]
         barButton.setTitleTextAttributes(attributes, for: .normal)
-//        let attributes = [NSAttributedString.Key.baselineOffset: NSNumber(value: -8)]
-
-//        barButton.setTitlePositionAdjustment(
-//            UIOffset(horizontal: 20, vertical: 0),
-//            for: UIBarMetrics.default
-//        )
 
         navBarItem.setLeftBarButton(
             barButton,
@@ -160,16 +130,18 @@ class SearchViewController: BaseViewController,
         self.navigationController?.dismiss(animated: true)
     }
 
-    @objc func onSearchReturn() {
-        self.searchBoxTextField.resignFirstResponder()
+    @objc func searchReturn() {
+        self.searchBar.resignFirstResponder()
 
-        let searchTerm = searchBoxTextField.text!
+        let searchTerm = searchBar.text!
         viewModel.trackSearchTerm(searchTerm: searchTerm)
 
-        if let url = URL(
-            string: "https://www.gov.uk/search/all?keywords=\(searchTerm)&order=relevance"
-        ) {
-            UIApplication.shared.open(url)
+        if searchTerm.isEmpty {
+            let blankSearchURL = "https://www.gov.uk/search?q="
+            UIApplication.shared.open(URL(string: blankSearchURL)!)
+        } else {
+            let searchTermURL = "https://www.gov.uk/search/all?keywords=\(searchTerm)&order=relevance"
+            UIApplication.shared.open(URL(string: searchTermURL)!)
         }
     }
 }

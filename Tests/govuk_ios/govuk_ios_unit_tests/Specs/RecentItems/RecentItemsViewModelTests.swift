2015002a1cef199df0f16c855b4fac74eec3c44e
raphael olumofe <raphaelo@hotmail.co.uk>
2024-09-06 22:21:25 +0100

    unit tests for viewModel and localizable files

---
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/RecentItems/RecentItemsViewModelTests.swift
@@ -0,0 +1,76 @@
+@testable import govuk_ios
+import XCTest
+
+final class RecentItemsViewModelTests: XCTestCase {
+    
+    var sut: RecentItemsViewModel?
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func test_state_whenIsSetToLoaded_loadsRecentItems() throws {
+        //Given
+       let mockRecentItemsService = MockRecentItemsService()
+       sut = RecentItemsViewModel(service: mockRecentItemsService)
+        
+        let recentItems = [[
+            RecentlyVisted(id: "ID",
+                 title: "Benefits",
+                 date: "2016-04-14T10:44:00+0000",
+                 url: "https://www.youtube.com/"),
+            RecentlyVisted(id: "ID",
+                 title: "Benefits fraud",
+                 date: "2016-04-14T10:44:00+0000",
+                 url: "https://www.youtube.com/"),
+            RecentlyVisted(id: "ID",
+                 title: "Universal",
+                 date: "2016-04-14T10:44:00+0000",
+                 url: "https://www.youtube.com/")],
+            [RecentlyVisted(id: "ID",
+                 title: "Driving",
+                 date: "2003-04-14T10:44:00+0000",
+                 url: "https://www.youtube.com/")],
+            [RecentlyVisted(id: "ID",
+                 title: "Benefits 2003",
+                 date: "2024-08-28T10:44:00+0000",
+                 url: "https://www.youtube.com/"),
+             RecentlyVisted(id: "ID",
+                 title: "Benefits 2003",
+                 date: "2024-08-29T10:44:00+0000",
+                 url: "https://www.youtube.com/")
+        ]]
+        //When
+        mockRecentItemsService._receivedfetchItemsCompletion?(.success(recentItems))
+    
+        //Then
+        XCTAssertEqual(sut?.state, .loaded(RecentlyViewedViewStructure(todaysItems: [], thisMonthsItems: [], otherMonthItems: recentItems)))
+    }
+    
+    func test_state_whenSetToError_loadsErrorMessage() throws {
+        //Given
+        let mockRecentItemsService = MockRecentItemsService()
+        sut = RecentItemsViewModel(service: mockRecentItemsService)
+        
+        //When
+        mockRecentItemsService._receivedfetchItemsCompletion?(.failure(.noRecentlyVisitedItems))
+        
+        //Then
+        XCTAssertEqual(sut?.state, .error(RecentItemsErrorStructure(
+            errorTitle: NSLocalizedString("errorViewTitle",bundle: .main, comment: ""),
+            errrorDesc: NSLocalizedString("errorViewDescription", bundle: .main,comment: ""))
+        )
+        )
+    }
+    
+    func test_state_whenFetchItemsCompletionIsNotCalled_isSetToLoading() throws {
+        
+        //Given
+        let mockRecentItemsService = MockRecentItemsService()
+        sut = RecentItemsViewModel(service: mockRecentItemsService)
+        
+        //Then
+        XCTAssertEqual(sut?.state, .loading )
+    }
+}
+

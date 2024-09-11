8ba2f372d2b6bdf1a9c952287dd5460afd8d5c36
Joshua Dubey <jodubey@deloitte.co.uk>
2024-09-11 14:13:36 +0100

    Clean up SettingsViewController layout code.  Remove test code from SettingsViewModel

--- Production/govuk_ios/ViewControllers/SettingsViewController.swift
+++ Production/govuk_ios/ViewControllers/SettingsViewController.swift
@@ -10,16 +10,15 @@ class SettingsViewController: BaseViewController,
 
     private var viewModel: SettingsViewModel
     private let scrollview = UIScrollView(frame: .zero)
+    private let backgroundColor = UIColor.govUK.fills.surfaceBackground
 
-    private var contentViewController: UIViewController {
-        let settingsContentView = GroupedList(content: viewModel.listContent)
+    private lazy var contentViewController: UIViewController = {
+        let settingsContentView = GroupedList(content: viewModel.listContent,
+                                              backgroundColor: backgroundColor)
         let viewController = UIHostingController(rootView: settingsContentView)
-        viewController.view.layer.backgroundColor = UIColor.clear.cgColor
-
-//        addChild(viewController)
-//        viewController.didMove(toParent: self)
+        viewController.view.translatesAutoresizingMaskIntoConstraints = false
         return viewController
-    }
+    }()
 
     public init(viewModel: SettingsViewModel) {
         self.viewModel = viewModel
@@ -33,68 +32,49 @@ class SettingsViewController: BaseViewController,
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("app version: ", viewModel.appVersion)
-
-        view.backgroundColor = UIColor.govUK.fills.surfaceBackground
-
+        view.backgroundColor = backgroundColor
         title = viewModel.title
         navigationController?.setNavigationBarHidden(false, animated: false)
         navigationController?.navigationBar.prefersLargeTitles = true
-
-        scrollview.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(scrollview)
 
         configureUI()
         configureConstraints()
     }
 
     private func configureUI() {
-        // placeholder content - will be replaced
-
-        let stack = UIStackView()
-        stack.axis = .vertical
-        stack.alignment = .top
-        stack.distribution = .fill
+        scrollview.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollview)
 
         addChild(contentViewController)
+
+        scrollview.addSubview(contentViewController.view)
+
         contentViewController.didMove(toParent: self)
-
-        stack.addArrangedSubview(contentViewController.view)
-
-        scrollview.addSubview(stack)
-
-//        scrollview.addSubview(contentViewController.view)
-//
-//        view.addSubview(scrollview)
-//        contentViewController.view
-//            .translatesAutoresizingMaskIntoConstraints = false
-//
-//        scrollview.addSubview(stack)
-
-        stack.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            stack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            stack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            stack.topAnchor.constraint(equalTo: scrollview.contentLayoutGuide.topAnchor),
-
-            stack.heightAnchor
-                .constraint(lessThanOrEqualTo: scrollview.contentLayoutGuide.heightAnchor)
-        ])
     }
 
     private func configureConstraints() {
         NSLayoutConstraint.activate([
-            view.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: scrollview.leadingAnchor),
-            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: scrollview.trailingAnchor),
-            view.safeAreaLayoutGuide.topAnchor.constraint(equalTo: scrollview.topAnchor),
-            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: scrollview.bottomAnchor),
+            contentViewController.view.leadingAnchor.constraint(equalTo:
+                                                scrollview.leadingAnchor),
+            contentViewController.view.trailingAnchor.constraint(equalTo:
+                                                scrollview.trailingAnchor),
+            contentViewController.view.topAnchor.constraint(equalTo:
+                                                scrollview.contentLayoutGuide.topAnchor,
+                                                constant: 10),
+            contentViewController.view.heightAnchor.constraint(lessThanOrEqualTo:
+                                                scrollview.contentLayoutGuide.heightAnchor),
+            view.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo:
+                                                scrollview.leadingAnchor),
+            view.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo:
+                                                scrollview.trailingAnchor),
+            view.safeAreaLayoutGuide.topAnchor.constraint(equalTo:
+                                                scrollview.topAnchor),
+            view.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo:
+                                                scrollview.bottomAnchor),
             scrollview.contentLayoutGuide
                 .leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             scrollview.contentLayoutGuide
                 .trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
-
-        print("app version: ", viewModel.appVersion)
     }
 }

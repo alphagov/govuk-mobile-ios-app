c01bacc9f51573d22b082ec98b362ad6406734a8
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-09-17 16:27:03 +0100

    Added tests

--- Production/govuk_ios/Extensions/Foundation/String+Extensions.swift
+++ Production/govuk_ios/Extensions/Foundation/String+Extensions.swift
@@ -2,35 +2,52 @@ import Foundation
 
 extension String {
     static var common: LocalStringBuilder {
-        .init(tableName: "Common")
+        .init(
+            tableName: "Common",
+            bundle: .main
+        )
     }
 
     static var home: LocalStringBuilder {
-        .init(tableName: "Home")
+        .init(
+            tableName: "Home",
+            bundle: .main
+        )
     }
 
     static var settings: LocalStringBuilder {
-        .init(tableName: "Settings")
+        .init(
+            tableName: "Settings",
+            bundle: .main
+        )
     }
 
     static var search: LocalStringBuilder {
-        .init(tableName: "Search")
+        .init(
+            tableName: "Search",
+            bundle: .main
+        )
     }
 
     static var onboarding: LocalStringBuilder {
-        .init(tableName: "Onboarding")
+        .init(
+            tableName: "Onboarding",
+            bundle: .main
+        )
     }
 }
 
 extension String {
     struct LocalStringBuilder {
         let tableName: String
+        let bundle: Bundle
 
         func localized(_ key: String,
                        comment: String = "") -> String {
             NSLocalizedString(
                 key,
                 tableName: tableName,
+                bundle: bundle,
                 comment: comment
             )
         }

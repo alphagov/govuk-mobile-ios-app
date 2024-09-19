305117ab73dec02232bfdd7909ee05732f2f279e
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-09-19 10:21:28 +0100

    Added concurreny support

--- Production/govuk_ios/Deeplinking/DeeplinkRoutes/DeeplinkRoute.swift
+++ Production/govuk_ios/Deeplinking/DeeplinkRoutes/DeeplinkRoute.swift
@@ -3,6 +3,8 @@ import UIKit
 
 protocol DeeplinkRoute {
     var pattern: URLPattern { get }
+
+    @MainActor
     func action(parent: BaseCoordinator,
                 params: [String: String])
 }

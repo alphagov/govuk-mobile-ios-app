81b5af9c511edec088350a62b45bf517d46c8ded
raphael olumofe <raphaelo@hotmail.co.uk>
2024-09-20 10:20:24 +0100

    Changed the test data URL to Gov.uk

--- Production/govuk_ios/Repositories/ActivityRepository.swift
+++ Production/govuk_ios/Repositories/ActivityRepository.swift
@@ -14,19 +14,19 @@ class ActivityRepository: ActivityRepositoryInterface {
         let activity = ActivityItem(context: coreData.backgroundContext)
         activity.id = UUID().uuidString
         activity.title = "benefit3"
-        activity.url = "https://www.youtube.com/"
+        activity.url = "https://www.gov.uk/"
         activity.date = Date()
 
         let activityTwo = ActivityItem(context: coreData.backgroundContext)
         activityTwo.id = UUID().uuidString
         activityTwo.title = "universal credit2"
-        activityTwo.url = "https://www.youtube.com/"
+        activityTwo.url = "https://www.gov.uk/"
         activityTwo.date = DateHelper.convertDateStringToDate(
             dateString: "2016-04-14T10:44:00+0000")
         let activityThree = ActivityItem(context: coreData.backgroundContext)
         activityThree.id = UUID().uuidString
         activityThree.title = "dvla2"
-        activityThree.url = "https://www.youtube.com/"
+        activityThree.url = "https://www.gov.uk/"
         activityThree.date = DateHelper.convertDateStringToDate(
             dateString: "2017-04-14T10:44:00+0000")
        try? coreData.backgroundContext.save()

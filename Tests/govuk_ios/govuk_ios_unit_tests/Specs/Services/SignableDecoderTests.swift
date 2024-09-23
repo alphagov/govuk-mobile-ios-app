e6643f439d010878366ad56dde73f77547dab3e8
Joshua Dubey <jodubey@deloitte.co.uk>
2024-09-23 15:17:01 +0100

    Adding tests

---
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Services/SignableDecoderTests.swift
@@ -0,0 +1,33 @@
+import Foundation
+import Testing
+@testable import govuk_ios
+
+struct SignableDecoderTests {
+
+    @Test
+    func test_verifyValidSignature() async throws {
+        let appConfigData = try #require(getMockConfigData(fileName: "MockAppConfigResponse"), "Unable to read mock data")
+        let appConfig = try #require(try? SignableDecoder().decode(AppConfig.self, from: appConfigData), "Unable to decode AppConfig")
+        #expect(appConfig != nil)
+    }
+    
+    @Test
+    func test_verifyInValidSignature() async throws {
+        let appConfigData = try #require(getMockConfigData(fileName: "MockAppConfigResponseInvalidSig"), "Unable to read mock data")
+        do {
+            _ = try SignableDecoder().decode(AppConfig.self, from: appConfigData)
+            #expect(Bool(false), "Expected to fail decoding")
+        } catch {
+            #expect(error is SigningError)
+        }
+    }
+    
+    private func getMockConfigData(fileName: String) -> Data? {
+        guard let resourceURL = Bundle.main.url(
+            forResource: fileName,
+            withExtension: "json"
+        ) else { return nil }
+        
+        return try? Data(contentsOf: resourceURL)
+    }
+}

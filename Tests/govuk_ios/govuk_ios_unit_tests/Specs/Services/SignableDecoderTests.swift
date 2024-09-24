35113c0094692fc419f8e7d47c1dcc85f8d24053
Joshua Dubey <jodubey@deloitte.co.uk>
2024-09-24 09:38:06 +0100

    Remove test_ from @Tests and mark as @Suite

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/Services/SignableDecoderTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Services/SignableDecoderTests.swift
@@ -2,17 +2,18 @@ import Foundation
 import Testing
 @testable import govuk_ios
 
+@Suite
 struct SignableDecoderTests {
 
     @Test
-    func test_verifyValidSignature() async throws {
+    func verifyValidSignature() async throws {
         let appConfigData = try #require(getMockConfigData(fileName: "MockAppConfigResponse"), "Unable to read mock data")
         let appConfig = try #require(try? SignableDecoder().decode(AppConfig.self, from: appConfigData), "Unable to decode AppConfig")
         #expect(appConfig != nil)
     }
     
     @Test
-    func test_verifyInValidSignature() async throws {
+    func verifyInValidSignature() async throws {
         let appConfigData = try #require(getMockConfigData(fileName: "MockAppConfigResponseInvalidSig"), "Unable to read mock data")
         do {
             _ = try SignableDecoder().decode(AppConfig.self, from: appConfigData)

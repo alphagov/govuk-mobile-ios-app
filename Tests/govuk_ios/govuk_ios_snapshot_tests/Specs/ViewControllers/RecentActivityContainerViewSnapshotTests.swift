d4aa4f01b4e23c5081e4069e98affb1e23d7813e
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-09-25 16:00:54 +0100

    Renamed RecentActivityContainerViewSnapshotTests

--- Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/RecentActivityContainerViewSnapshotTests.swift
+++ Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/RecentActivityContainerViewSnapshotTests.swift
@@ -5,7 +5,7 @@ import CoreData
 
 @testable import govuk_ios
 
-class RecentActvitySnapshotTests: SnapshotTestCase {
+class RecentActivityContainerViewSnapshotTests: SnapshotTestCase {
     func test_loadInNavigationController_light_errorView_rendersCorrectly() {
         let coreData = CoreDataRepository.arrange(
             notificationCenter: .default
@@ -113,4 +113,3 @@ class RecentActvitySnapshotTests: SnapshotTestCase {
         )
     }
 }
-

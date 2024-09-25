acae99a3cd0f67a2261658e51ed1796d8063837f
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-09-25 08:26:55 +0100

    Added search results snapshot

--- Production/govuk_ios/SupportingFiles/main.swift
+++ Production/govuk_ios/SupportingFiles/main.swift
@@ -7,6 +7,8 @@ let klass: AnyClass = NSClassFromString("govuk_ios_unit_tests.TestAppDelegate") 
 let classString = NSStringFromClass(klass)
 
 let rawPointer = UnsafeMutableRawPointer(CommandLine.unsafeArgv)
-let argv = rawPointer.bindMemory(to: UnsafeMutablePointer<Int8>.self,
-                                 capacity: Int(CommandLine.argc))
+let argv = rawPointer.bindMemory(
+    to: UnsafeMutablePointer<Int8>.self,
+    capacity: Int(CommandLine.argc)
+)
 _ = UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, classString)
--- Tests/govuk_ios/shared/Mocks/MockSearchService.swift
+++ Tests/govuk_ios/shared/Mocks/MockSearchService.swift
@@ -4,10 +4,14 @@ import Foundation
 
 class MockSearchService: SearchServiceInterface {    
     var _searchReceivedTerm: String?
+    var _stubbedSearchResult: SearchResult?
     var _searchReceivedCompletion: ((Result<SearchResult, SearchError>) -> Void)?
     func search(_ term: String,
                 completion: @escaping (Result<SearchResult, SearchError>) -> Void) {
         _searchReceivedTerm = term
         _searchReceivedCompletion = completion
+        if let result = _stubbedSearchResult {
+            completion(.success(result))
+        }
     }
 }

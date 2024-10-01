0e7d2cf26126bcb97ab359b20a5b59ab401fd1cf
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-10-01 12:50:38 +0100

    Save activity

--- Production/govuk_ios/Repositories/ActivityRepository.swift
+++ Production/govuk_ios/Repositories/ActivityRepository.swift
@@ -1,6 +1,8 @@
 import Foundation
+import CoreData
 
 protocol ActivityRepositoryInterface {
+    func create(params: ActivityItemCreateParams)
 }
 
 class ActivityRepository: ActivityRepositoryInterface {
@@ -8,5 +10,35 @@ class ActivityRepository: ActivityRepositoryInterface {
 
     init(coreData: CoreDataRepositoryInterface) {
         self.coreData = coreData
+    }
+
+    func create(params: ActivityItemCreateParams) {
+        let localContext = coreData.backgroundContext
+        let local = get(id: params.url, context: localContext) ??
+        ActivityItem(context: localContext)
+        local.update(params)
+        try? localContext.save()
+    }
+
+    private func get(id: String,
+                     context: NSManagedObjectContext?) -> ActivityItem? {
+        fetch(
+            predicate: .init(format: "url = %@", id),
+            context: context
+        ).fetchedObjects?.first
+    }
+
+    private func fetch(predicate: NSPredicate?,
+                       context: NSManagedObjectContext?)
+    -> NSFetchedResultsController<ActivityItem> {
+        let fetchContext = context ?? coreData.viewContext
+        let request = ActivityItem.fetchRequest()
+        request.predicate = predicate
+        return NSFetchedResultsController<ActivityItem>(
+            fetchRequest: request,
+            managedObjectContext: fetchContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        ).fetch()
     }
 }
--- Production/govuk_ios/Services/ActivityService.swift
+++ Production/govuk_ios/Services/ActivityService.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 protocol ActivityServiceInterface {
+    func save(searchItem: SearchItem)
 }
 
 struct ActivityService: ActivityServiceInterface {
@@ -8,5 +9,28 @@ struct ActivityService: ActivityServiceInterface {
 
     init(repository: ActivityRepositoryInterface) {
         self.repository = repository
+    }
+
+    func save(searchItem: SearchItem) {
+        let params = ActivityItemCreateParams(
+            searchItem: searchItem
+        )
+        repository.create(params: params)
+    }
+}
+
+struct ActivityItemCreateParams {
+   var id: String
+   var title: String
+   var date: Date
+   var url: String
+}
+
+extension ActivityItemCreateParams {
+    init(searchItem: SearchItem) {
+        self.id = searchItem.link
+        self.title = searchItem.title
+        self.date = Date()
+        self.url = searchItem.link
     }
 }

da62255ecf796dc841926998e32fa317561b7b4a
TimDenyer <timothy.denyer@digital.cabinet-office.gov.uk>
2024-10-08 16:42:10 +0100

    Update accessibility label as requested on GOVUKAPP-589

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/ViewModels/AppUnavailableContainerViewModelTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/ViewModels/AppUnavailableContainerViewModelTests.swift
@@ -12,7 +12,7 @@ struct AppUnavailableViewModelTests {
         #expect(sut.title == "Sorry, the app is unavailable")
         #expect(sut.subheading == "You cannot use the GOV.UK app at the moment. Try again later.")
         #expect(sut.goToGovUkButtonTitle == "Go to GOV.UK ↗")
-        #expect(sut.goToGovUkAccessibilityButtonTitle == "Go to GOV.UK")
+        #expect(sut.goToGovUkAccessibilityButtonTitle == "Go to the GOV.UK website")
         #expect(sut.goToGovUkAccessibilityButtonHint == "Opens in web browser")
         #expect(sut.govUkUrl == "https://www.gov.uk")
     }

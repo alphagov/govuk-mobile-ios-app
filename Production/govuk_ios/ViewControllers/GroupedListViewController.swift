d7236c6a4232698504eb72e6353f4fe803a07278
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-10-10 14:37:32 +0100

    Fixed files

--- Production/govuk_ios/ViewControllers/GroupedListViewController.swift
+++ Production/govuk_ios/ViewControllers/GroupedListViewController.swift
@@ -1,75 +1,17 @@
 import Foundation
 import UIKit
 
-private typealias DataSource = UITableViewDiffableDataSource<ActivitySection, ActivityItem>
-private typealias Snapshot = NSDiffableDataSourceSnapshot<ActivitySection, ActivityItem>
-
-class GroupedListViewModel {
-    private let activityService: ActivityServiceInterface
-    private let recentActivityHeaderFormatter = DateFormatter.recentActivityHeader
-    private(set) var structure: RecentActivitiesViewStructure = .init(
-        todaysActivites: [],
-        currentMonthActivities: [],
-        recentMonthActivities: [:]
-    )
-
-    init(activityService: ActivityServiceInterface) {
-        self.activityService = activityService
-    }
-
-    @discardableResult
-    func fetchActivities() -> RecentActivitiesViewStructure {
-        let items = activityService.fetch().fetchedObjects ?? []
-        let localStructure = sortActivites(activities: items)
-        structure = localStructure
-        return localStructure
-    }
-
-    private func sortActivites(activities: [ActivityItem]) -> RecentActivitiesViewStructure {
-        var todaysActivities: [ActivityItem] = []
-        var currentMonthActivities: [ActivityItem] = []
-        var recentMonthsActivities: [MonthGroupKey: [ActivityItem]] = [:]
-        for recentActivity in activities {
-            if recentActivity.date.isToday() {
-                todaysActivities.append(recentActivity)
-            } else if recentActivity.date.isThisMonth() {
-                currentMonthActivities.append(recentActivity)
-            } else {
-                let key = MonthGroupKey(
-                    date: recentActivity.date,
-                    formatter: recentActivityHeaderFormatter
-                )
-                var items = recentMonthsActivities[key] ?? []
-                items.append(recentActivity)
-                recentMonthsActivities[key] = items
-            }
-        }
-
-        return .init(
-            todaysActivites: todaysActivities,
-            currentMonthActivities: currentMonthActivities,
-            recentMonthActivities: recentMonthsActivities
-        )
-    }
-}
+private typealias DataSource = UITableViewDiffableDataSource<GroupListSection, GroupListItem>
+private typealias Snapshot = NSDiffableDataSourceSnapshot<GroupListSection, GroupListItem>
 
 class GroupedListViewController: UIViewController,
                                  UITableViewDelegate {
     private lazy var tableView: UITableView = UITableView.groupedList
+    private let lastVisitedFormatter = DateFormatter.recentActivityLastVisited
     private lazy var dataSource: DataSource = {
         let localDataSource = DataSource(
             tableView: tableView,
-            cellProvider: { tableView, indexPath, item in
-                let cell: GroupedListTableViewCell = tableView.dequeue(indexPath: indexPath)
-                let section = self.viewModel.structure.sections[indexPath.section]
-                cell.configure(
-                    title: item.title,
-                    description: item.body,
-                    top: indexPath.row == 0,
-                    bottom: item == section.items.last
-                )
-                return cell
-            }
+            cellProvider: loadCell
         )
         localDataSource.defaultRowAnimation = .fade
         return localDataSource
@@ -137,32 +79,41 @@ class GroupedListViewController: UIViewController,
         return localLabel
     }
 
-//    func tableView(_ tableView: UITableView,
-//                   heightForHeaderInSection
-//                   section: Int) -> CGFloat {
-//        24
-//    }
-//
-//    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
-//        <#code#>
-//    }
-}
-
-struct ActivitySection: Hashable {
-    let title: String
-    let items: [ActivityItem]
-}
-
-extension UITableView {
-    static var groupedList: UITableView {
-        let tableView = UITableView(frame: .zero, style: .grouped)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.register(GroupedListTableViewCell.self)
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 60
-        tableView.estimatedSectionHeaderHeight = 24
-        tableView.separatorStyle = .none
-        tableView.backgroundColor = .clear
-        return tableView
+    private var loadCell: (UITableView, IndexPath, GroupListItem) -> GroupedListTableViewCell {
+        return { [weak self] tableView, indexPath, item in
+            let cell: GroupedListTableViewCell = tableView.dequeue(indexPath: indexPath)
+            if let section = self?.viewModel.structure.sections[indexPath.section] {
+                cell.configure(
+                    title: item.activity.title,
+                    description: self?.lastVisitedString(activity: item.activity),
+                    top: indexPath.row == 0,
+                    bottom: item == section.items.last
+                )
+            }
+            return cell
+        }
     }
+
+    private func lastVisitedString(activity: ActivityItem) -> String {
+        let copy = String.recentActivity.localized(
+            "recentActivityFormattedDateStringComponent"
+        )
+        let formattedDateString = lastVisitedFormatter.string(from: activity.date)
+        return "\(copy) \(formattedDateString)"
+    }
+
+//    private var selected: (ActivityItem) -> Void {
+//        return { item in
+//
+//        }
+//    }
+}
+
+struct GroupListSection: Hashable {
+    let title: String
+    let items: [GroupListItem]
+}
+
+struct GroupListItem: Hashable {
+    let activity: ActivityItem
 }

98badaea40199ccde340d8627c89869684b2e060
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-10-11 15:46:10 +0100

    Added snapshot tests

--- Tests/govuk_ios/govuk_ios_unit_tests/Mocks/Repositories/MockActivityRepository.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Mocks/Repositories/MockActivityRepository.swift
@@ -1,12 +1,23 @@
 import Foundation
+import CoreData
 
 @testable import govuk_ios
 
-class MockActivityRepository: ActivityRepositoryInterface {
+class MockActivityRepository: ActivityRepositoryInterface {    
 
     var _receivedSaveParams: ActivityItemCreateParams?
     func save(params: ActivityItemCreateParams) {
         _receivedSaveParams = params
+    }
+
+    var _stubbedFetchResultsController: NSFetchedResultsController<ActivityItem>!
+    func fetch() -> NSFetchedResultsController<ActivityItem> {
+        _stubbedFetchResultsController
+    }
+
+    var _receivedDeleteAll: Bool = false
+    func deleteAll() {
+        _receivedDeleteAll = true
     }
 
 }
--- Tests/govuk_ios/shared/Mocks/Services/MockActivityService.swift
+++ Tests/govuk_ios/shared/Mocks/Services/MockActivityService.swift
@@ -1,11 +1,22 @@
 import Foundation
+import  CoreData
 
 @testable import govuk_ios
 
 class MockActivityService: ActivityServiceInterface {
-
     var _receivedSaveSearchItem: SearchItem?
     func save(searchItem: SearchItem) {
         _receivedSaveSearchItem = searchItem
     }
+
+    var _stubbedFetchResultsController: NSFetchedResultsController<ActivityItem>!
+    func fetch() -> NSFetchedResultsController<ActivityItem> {
+        _stubbedFetchResultsController
+    }
+
+    var _receivedDeleteAll: Bool = false
+    func deleteAll() {
+        _receivedDeleteAll = true
+    }
+
 }

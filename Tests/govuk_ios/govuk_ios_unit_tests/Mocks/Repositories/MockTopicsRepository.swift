3f790256ecafc72f4ffddf21d461c75fb61fe08c
Joshua Dubey <jodubey@deloitte.co.uk>
2024-10-14 10:54:09 +0100

    Add tests for EditTopicsViewModel and TopicsRepository

--- Tests/govuk_ios/govuk_ios_unit_tests/Mocks/Repositories/MockTopicsRepository.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Mocks/Repositories/MockTopicsRepository.swift
@@ -4,20 +4,25 @@ import Foundation
 
 class MockTopicsRepository: TopicsRepositoryInterface {
     
+    var _didCallSaveTopicsList = false
     func saveTopicsList(_ topicResponses: [TopicResponseItem]) {
-        
+        self._didCallSaveTopicsList = true
     }
     
+    var _didCallFetchFavorites = false
     func fetchFavoriteTopics() -> [Topic] {
-        []
+        self._didCallFetchFavorites = true
+        return []
     }
     
+    var _didCallFetchAll = false
     func fetchAllTopics() -> [Topic] {
-        []
+        self._didCallFetchAll = true
+        return []
     }
     
-    
+    var _didCallSaveChanges = false
     func saveChanges() {
-        
+        self._didCallSaveChanges = true
     }
 }

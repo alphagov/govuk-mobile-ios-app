5f874288e827d625924658a962adb74f16c30087
joshdubey <95635393+joshdubey@users.noreply.github.com>
2024-10-17 09:37:58 +0100

    Use default (opaque) appearance for tab bar (#128)

--- Production/govuk_ios/Extensions/UIKit/UITabBarAppearance+GOVUK.swift
+++ Production/govuk_ios/Extensions/UIKit/UITabBarAppearance+GOVUK.swift
@@ -4,7 +4,6 @@ import UIKit
 extension UITabBarAppearance {
     static var govUK: UITabBarAppearance {
         let appearance = UITabBarAppearance()
-        appearance.configureWithTransparentBackground()
         appearance.shadowColor = UIColor.govUK.strokes.listDivider
         appearance.backgroundColor = UIColor.govUK.fills.surfaceFixedContainer
         appearance.inlineLayoutAppearance = .govUK

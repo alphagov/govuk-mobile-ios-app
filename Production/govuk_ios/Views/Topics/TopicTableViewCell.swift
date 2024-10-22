52c099868abb2adacd7ff4bb62b44578b81802ea
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2024-10-22 12:46:17 +0100

    Remove unused init

--- Production/govuk_ios/Views/Topics/TopicTableViewCell.swift
+++ Production/govuk_ios/Views/Topics/TopicTableViewCell.swift
@@ -4,14 +4,6 @@ import Foundation
 class TopicTableViewCell: UITableViewCell {
     var topicCard: UIView = UIView()
 
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     func configure(topic: Topic, viewModel: AllTopicsViewModel) {
         selectionStyle = .none
         contentView.backgroundColor = UIColor.govUK.fills.surfaceBackground

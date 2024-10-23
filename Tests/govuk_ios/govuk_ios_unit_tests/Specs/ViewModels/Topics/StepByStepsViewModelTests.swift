a078186bb582e65cb2208be756b7d9cf5e7527bb
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-10-23 07:14:50 +0100

    Added StepByStepsViewModelTests

---
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/ViewModels/Topics/StepByStepsViewModelTests.swift
@@ -0,0 +1,24 @@
+import Testing
+
+@testable import govuk_ios
+
+struct StepByStepsViewModelTests {
+
+    let mockTopicsService = MockTopicsService()
+    let mockAnalyticsService = MockAnalyticsService()
+    let mockActivityService = MockActivityService()
+    let mockURLOpener = MockURLOpener()
+
+    @Test
+    func init_hasCorrectParameters() {
+        let sut = StepByStepsViewModel(
+            content: [],
+            analyticsService: mockAnalyticsService,
+            activityService: mockActivityService,
+            urlOpener: mockURLOpener
+        )
+        #expect(sut.title == "Step by step guides")
+        #expect(!sut.shouldShowDescription)
+        #expect(sut.sections.count == 1)
+    }
+}

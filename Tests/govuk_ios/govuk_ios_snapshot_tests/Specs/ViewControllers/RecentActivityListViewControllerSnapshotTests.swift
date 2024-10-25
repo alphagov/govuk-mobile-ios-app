ae82014f61f915d579a9d09ec253bdbdb6c24f12
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-10-25 20:58:26 +0100

    Added more calls in RecentActivityListViewControllerSnapshotTests

--- Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/RecentActivityListViewControllerSnapshotTests.swift
+++ Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/RecentActivityListViewControllerSnapshotTests.swift
@@ -159,9 +159,57 @@ final class RecentActivityListViewControllerSnapshotTests: SnapshotTestCase {
         viewController.setEditing(true, animated: false)
 
         let tableview = viewController.view.subviews.compactMap { $0 as? UITableView }.first!
+        tableview.selectAllRows(animated: false)
+
+        tableview.deselectAllRows(animated: false)
         let index = IndexPath(item: 0, section: 0)
         tableview.selectRow(at: index, animated: false, scrollPosition: .none)
         viewController.tableView(tableview, didSelectRowAt: index)
+
+        VerifySnapshotInWindow(
+            navigationController
+        )
+    }
+
+    func test_loadInNavigationController_editing_allSelected_rendersCorrectly() {
+        let mockActivityService = MockActivityService()
+        let coreData = CoreDataRepository.arrangeAndLoad
+        _ = ActivityItem.arrange(
+            title: "Test 1",
+            date: .arrange("21/12/2024"),
+            context: coreData.viewContext
+        )
+
+        _ = ActivityItem.arrange(
+            title: "Test 2",
+            date: .arrange("10/01/2023"),
+            context: coreData.viewContext
+        )
+        let request = ActivityItem.fetchRequest()
+        let resultsController = NSFetchedResultsController<ActivityItem>(
+            fetchRequest: request,
+            managedObjectContext: coreData.viewContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+        resultsController.fetch()
+        mockActivityService._stubbedFetchResultsController = resultsController
+
+        let viewController = RecentActivityListViewController(
+            viewModel: .init(
+                activityService: mockActivityService,
+                analyticsService: MockAnalyticsService(),
+                urlopener: MockURLOpener()
+            )
+        )
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.overrideUserInterfaceStyle = .light
+
+        viewController.setEditing(true, animated: false)
+
+        let tableview = viewController.view.subviews.compactMap { $0 as? UITableView }.first!
+        tableview.selectAllRows(animated: false)
 
         VerifySnapshotInWindow(
             navigationController

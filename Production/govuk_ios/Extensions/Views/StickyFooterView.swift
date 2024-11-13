292c18f27d0983b0f8344c03b6fda8a0758a94a0
raphaelolu <47435245+raphaelolu@users.noreply.github.com>
2024-11-13 09:36:00 +0000

    [GOVUKAPP-897] - Topic Onboarding button resizing (#170)
    * creates sticky footer view and fixes the button resizing bug in the topic onboarding viewcontroller
    
    * Fixed comments
    
    ---------
    
    Co-authored-by: Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>

---
+++ Production/govuk_ios/Extensions/Views/StickyFooterView.swift
@@ -0,0 +1,74 @@
+import Foundation
+import UIKit
+
+class StickyFooterView: UIView {
+    private lazy var stackView: UIStackView = {
+        let localView = UIStackView()
+        localView.translatesAutoresizingMaskIntoConstraints = false
+        localView.axis = .vertical
+        localView.distribution = .fillEqually
+        localView.spacing = 4
+        return localView
+    }()
+
+    private lazy var divider: UIView = {
+        let localView = UIView()
+        localView.backgroundColor = UIColor.govUK.strokes.listDivider
+        localView.translatesAutoresizingMaskIntoConstraints = false
+        return localView
+    }()
+
+    init() {
+        super.init(frame: .zero)
+        configureUI()
+        configureConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func configureUI() {
+        layoutMargins = .init(all: 16)
+        addSubview(divider)
+        addSubview(stackView)
+    }
+
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            divider.heightAnchor.constraint(
+                equalToConstant: 0.33
+            ),
+            divider.trailingAnchor.constraint(
+                equalTo: trailingAnchor
+            ),
+            divider.leadingAnchor.constraint(
+                equalTo: leadingAnchor
+            ),
+            divider.topAnchor.constraint(
+                equalTo: topAnchor
+            ),
+            stackView.topAnchor.constraint(
+                equalTo: divider.bottomAnchor,
+                constant: 16
+            ),
+            stackView.leadingAnchor.constraint(
+                equalTo: layoutMarginsGuide.leadingAnchor
+            ),
+            stackView.trailingAnchor.constraint(
+                equalTo: layoutMarginsGuide.trailingAnchor
+            ),
+            stackView.bottomAnchor.constraint(
+                equalTo: layoutMarginsGuide.bottomAnchor
+            )
+        ])
+    }
+
+    func addView(_ view: UIView) {
+        stackView.addArrangedSubview(view)
+    }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        stackView.axis = traitCollection.verticalSizeClass == .compact ? .horizontal : .vertical
+    }
+}
--- Production/govuk_ios/ViewControllers/TopicOnboardingViewController.swift
+++ Production/govuk_ios/ViewControllers/TopicOnboardingViewController.swift
@@ -2,11 +2,12 @@ import Foundation
 import UIKit
 import SwiftUI
 import UIComponents
+import Combine
 
 class TopicOnboardingViewController: BaseViewController,
                                      TrackableScreen {
     var trackingName: String { "Select relevant topics" }
-
+    private var cancellables = Set<AnyCancellable>()
     private let viewModel: TopicOnboardingViewModel
 
     init(viewModel: TopicOnboardingViewModel) {
@@ -67,6 +68,7 @@ class TopicOnboardingViewController: BaseViewController,
         configureUI()
         configureConstraints()
         registerObservers()
+        addSubscriptions()
     }
 
     private func registerObservers() {
@@ -91,18 +93,42 @@ class TopicOnboardingViewController: BaseViewController,
         view.backgroundColor = UIColor.govUK.fills.surfaceBackground
         view.addSubview(scrollView)
         scrollView.addSubview(stackView)
-        view.addSubview(buttonView)
+        view.addSubview(footerView)
         stackView.addArrangedSubview(subtitleLabel)
         stackView.addArrangedSubview(topicsListView)
         topicsListView.updateTopics(viewModel.topics)
     }
 
-    private lazy var buttonView: UIView =  {
-        let controller = UIHostingController(
-            rootView: TopicsButtonView(viewModel: viewModel)
+    private lazy var footerView: UIView = {
+        let localView = StickyFooterView()
+        localView.translatesAutoresizingMaskIntoConstraints = false
+        localView.addView(primaryButton)
+        localView.addView(secondaryButton)
+        return localView
+    }()
+
+    private lazy var primaryButton: UIButton = {
+        let localView = GOVUKButton(
+            .primary,
+            viewModel: viewModel.primaryButtonViewModel
         )
-        controller.view.translatesAutoresizingMaskIntoConstraints = false
-        return controller.view
+        localView.translatesAutoresizingMaskIntoConstraints = false
+        return localView
+    }()
+
+    private func addSubscriptions() {
+        viewModel.$isTopicSelected.sink(
+            receiveValue: primaryButton.setEnabled
+        ).store(in: &cancellables)
+    }
+
+    private lazy var secondaryButton: UIView = {
+        let localView = GOVUKButton(
+            .secondary,
+            viewModel: viewModel.secondaryButtonViewModel
+        )
+        localView.translatesAutoresizingMaskIntoConstraints = false
+        return localView
     }()
 
     private func configureConstraints() {
@@ -122,23 +148,23 @@ class TopicOnboardingViewController: BaseViewController,
             stackView.widthAnchor.constraint(
                 equalTo: view.layoutMarginsGuide.widthAnchor
             ),
-            buttonView.topAnchor.constraint(
+            footerView.topAnchor.constraint(
                 equalTo: scrollView.bottomAnchor
             ),
-            buttonView.bottomAnchor.constraint(
+            footerView.bottomAnchor.constraint(
                 equalTo: view.bottomAnchor
             ),
-            buttonView.leftAnchor.constraint(
+            footerView.leftAnchor.constraint(
                 equalTo: view.leftAnchor
             ),
-            buttonView.rightAnchor.constraint(
+            footerView.rightAnchor.constraint(
                 equalTo: view.rightAnchor
             ),
             scrollView.topAnchor.constraint(
                 equalTo: view.topAnchor
             ),
             scrollView.bottomAnchor.constraint(
-                equalTo: buttonView.topAnchor
+                equalTo: footerView.topAnchor
             ),
             scrollView.leftAnchor.constraint(
                 equalTo: view.layoutMarginsGuide.leftAnchor

4bf4f648f2f62147e8333ba19738af6deeaca594
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-11-20 10:45:38 +0000

    Added more CrashlyticsClientTests

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/Clients/CrashlyticsClientTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Clients/CrashlyticsClientTests.swift
@@ -28,6 +28,57 @@ struct CrashlyticsClientTests {
 
         #expect(mockCrashlytics._setCrashlyticsCollectionEnabledReceivedEnabled == false)
     }
+
+    @Test
+    func launch_exists_doesNothing() {
+        let mockCrashlytics = MockCrashlytics()
+        let sut = CrashlyticsClient(
+            crashlytics: mockCrashlytics
+        )
+
+        sut.launch()
+
+        #expect(mockCrashlytics._setCrashlyticsCollectionEnabledReceivedEnabled == nil)
+    }
+
+    @Test
+    func trackScreen_exists_doesNothing() {
+        let mockCrashlytics = MockCrashlytics()
+        let sut = CrashlyticsClient(
+            crashlytics: mockCrashlytics
+        )
+
+        let expectedScreen = MockBaseViewController()
+        sut.track(screen: expectedScreen)
+
+        #expect(mockCrashlytics._setCrashlyticsCollectionEnabledReceivedEnabled == nil)
+    }
+
+    @Test
+    @MainActor
+    func trackEvent_exists_doesNothing() {
+        let mockCrashlytics = MockCrashlytics()
+        let sut = CrashlyticsClient(
+            crashlytics: mockCrashlytics
+        )
+
+        let expectedScreen = MockBaseViewController()
+        sut.track(event: .init(name: "test", params: nil))
+
+        #expect(mockCrashlytics._setCrashlyticsCollectionEnabledReceivedEnabled == nil)
+    }
+
+    @Test
+    func setUserProperty_exists_doesNothing() {
+        let mockCrashlytics = MockCrashlytics()
+        let sut = CrashlyticsClient(
+            crashlytics: mockCrashlytics
+        )
+
+        sut.set(userProperty: .init(key: "test", value: "test"))
+
+        #expect(mockCrashlytics._setCrashlyticsCollectionEnabledReceivedEnabled == nil)
+    }
 }
 
 class MockCrashlytics: CrashlyticsInterface {

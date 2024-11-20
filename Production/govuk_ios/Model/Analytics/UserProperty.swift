1abfae734227b6093ddf0579ab426a679cfc69b5
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-11-20 15:45:17 +0000

    Split out UserProperty+Convenience

--- Production/govuk_ios/Model/Analytics/UserProperty.swift
+++ Production/govuk_ios/Model/Analytics/UserProperty.swift
@@ -4,12 +4,3 @@ struct UserProperty {
     let key: String
     let value: String?
 }
-
-extension UserProperty {
-    static var topicsCustomised: UserProperty {
-        .init(
-            key: "topics_customised",
-            value: "true"
-        )
-    }
-}

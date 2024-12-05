c4de215b19f06b08f915f577148303bca951041a
raphaelolu <47435245+raphaelolu@users.noreply.github.com>
2024-12-05 12:29:04 +0000

    Long navigation titles for topic detail views (#202)
    * title scrolls with content
    
    * moved title inside v stack
    
    * updates snapshot tests
    
    * removed previous extra padding
    
    * Deleted empty helpers file
    
    * adjusted padding
    
    * adjusted padding
    
    * adjusted padding

--- Production/govuk_ios/SwiftUIViews/Helpers/ScrollOffsetPreferenceKey.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-struct ScrollOffsetPreferenceKey: PreferenceKey {
-    static var defaultValue: CGFloat = 0.0
-    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
-        value += nextValue()
-    }
-}

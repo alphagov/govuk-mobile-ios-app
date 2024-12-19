6bb1a76125146997977312d8d67968ce8140c797
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2024-12-19 14:14:13 +0000

    Added RedactorTests

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/Redactors/RedactorTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Redactors/RedactorTests.swift
@@ -8,33 +8,10 @@ import Testing
 struct RedactorTests {
     @Test
     func redact_returnsExpectedResult() {
-        let mockRedactor = MockRedactor()
-        mockRedactor._stubbedRedactedString = UUID().uuidString
-        let sut = CompoundRedactor(
-            redactors: [
-                mockRedactor
-            ]
-        )
-        let text = """
-        the cat in the hat
-        """
-        let result = sut.redact(text)
-        #expect(mockRedactor._receivedInputText == text)
-        #expect(result == mockRedactor._stubbedRedactedString)
-    }
+        let string = UUID().uuidString
+        let sut = Redactor()
+        let result = sut.redact(string)
 
-    @Test
-    func pii_redact_returnsExpectedResult() {
-        let sut = Redactor.pii
-        let text = """
-        Hi my postcode is FL52HP and my email is test@gmail.com.
-        I have an insurance number: QQ123456B
-        """
-        let result = sut.redact(text)
-        let expected = """
-        Hi my postcode is [postcode] and my email is [email].
-        I have an insurance number: [NI number]
-        """
-        #expect(result == expected)
+        #expect(result == string)
     }
 }

26ed67093998cbf936f8ef904f85fad9d75c936c
raphael olumofe <raphaelo@hotmail.co.uk>
2024-12-20 12:05:02 +0000

    add space

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/Helpers/StringValidator/MaximumLengthValidatorTest.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Helpers/StringValidator/MaximumLengthValidatorTest.swift
@@ -9,6 +9,7 @@ struct MaximumLengthValidatorTest {
     @Test
     func validate_whenTextInputIsLessOrEqualThanRequiredStringLength_returnsTrue() async throws {
         let sut = MaximumLengthValidator(requiredStringLength: 5)
+        
         let testStringOne = "Seven"
 
         #expect(sut.validate(input: testStringOne) == true)
@@ -21,6 +22,7 @@ struct MaximumLengthValidatorTest {
     @Test
     func validate_whenTextInputIsGreaterThanRequiredStringLength_returnsFalse() async throws {
         let sut = MaximumLengthValidator(requiredStringLength: 5)
+
         let testString = "sentence"
 
         #expect(sut.validate(input: testString) == false)

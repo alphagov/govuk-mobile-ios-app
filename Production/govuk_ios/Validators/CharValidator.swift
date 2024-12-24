0c3f823c808a15ce730788aef77bbf153d9125b4
raphael olumofe <raphaelo@hotmail.co.uk>
2024-12-24 01:06:11 +0000

    white space

--- Production/govuk_ios/Validators/CharValidator.swift
+++ Production/govuk_ios/Validators/CharValidator.swift
@@ -4,7 +4,7 @@ struct CharValidator: ValidatorProvider {
     func validate(input: String?) -> Bool {
         guard let input = input else { return false }
         let invalidChars: [String] = ["}", "*", "-", "{"]
-        
+
         return invalidChars.filter { input.contains($0) }.count == 0
     }
 }

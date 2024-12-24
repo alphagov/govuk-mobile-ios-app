1ee3ddec6f7f22e76b965cf8e4357c9f9188ac26
raphael olumofe <raphaelo@hotmail.co.uk>
2024-12-24 00:29:48 +0000

    updated char validator method and test file structure

--- Production/govuk_ios/Validators/CharValidator.swift
+++ Production/govuk_ios/Validators/CharValidator.swift
@@ -4,9 +4,7 @@ struct CharValidator: ValidatorProvider {
     func validate(input: String?) -> Bool {
         guard let input = input else { return false }
         let invalidChars: [String] = ["}", "*", "-", "{"]
-
-         return invalidChars.filter { char in
-            input.contains(char)
-         }.count == 0
+        
+        return invalidChars.filter { input.contains($0) }.count == 0
     }
 }

3f3bb20a35e8a63b33bbad265aab542ef0b4d4f2
raphael olumofe <raphaelo@hotmail.co.uk>
2025-01-08 11:41:30 +0000

    removed test view controller, viewmodel and tests

--- Tests/govuk_ios/govuk_ios_unit_tests/Mocks/Builders/MockViewControllerBuilder.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Mocks/Builders/MockViewControllerBuilder.swift
@@ -10,29 +10,12 @@ extension ViewControllerBuilder {
 }
 
 class MockViewControllerBuilder: ViewControllerBuilder {
-    var _stubbedDrivingViewController: UIViewController?
-    var _receivedDrivingShowPermitAction: (() -> Void)?
-    var _receivedDrivingPresentPermitAction: (() -> Void)?
-    override func driving(showPermitAction: @escaping () -> Void,
-                          presentPermitAction: @escaping () -> Void) -> UIViewController {
-        _receivedDrivingShowPermitAction = showPermitAction
-        _receivedDrivingPresentPermitAction = presentPermitAction
-        return _stubbedDrivingViewController ?? UIViewController()
-    }
 
     var _stubbedLaunchViewController: UIViewController?
     var _receivedLaunchCompletion: (() -> Void)?
     override func launch(completion: @escaping () -> Void) -> UIViewController {
         _receivedLaunchCompletion = completion
         return _stubbedLaunchViewController ?? UIViewController()
-    }
-
-    var _stubbedPermitViewController: UIViewController?
-    var _receivedPermitFinishAction: (() -> Void)?
-    override func permit(permitId: String,
-                         finishAction: @escaping () -> Void) -> UIViewController {
-        _receivedPermitFinishAction = finishAction
-        return _stubbedPermitViewController ?? UIViewController()
     }
 
     var _stubbedHomeViewController: UIViewController?

aa2cb9d64b83db5b5cb0b2f370d1a423673a06dc
Joshua Dubey <jodubey@deloitte.co.uk>
2025-01-22 13:51:50 +0000

    Refactor AppUnavailableContainer to handle multiple error types Inject the AppLaunchService instead of AppConfigService, so it can be invoked by Try again action

--- Production/govuk_ios/SwiftUIViews/AppUnavailableContainerView.swift
+++ Production/govuk_ios/SwiftUIViews/AppUnavailableContainerView.swift
@@ -16,25 +16,25 @@ struct AppUnavailableContainerView: View {
                        subheading: viewModel.subheading)
             .padding(.top, verticalSizeClass == .compact ? 30 : 46)
             .padding(.horizontal, 16)
+            ProgressView()
+                .controlSize(.large)
+                .opacity(viewModel.showProgressView ? 1 : 0)
+                .padding(.top, 32)
             Spacer()
             Divider()
                 .foregroundColor(Color(UIColor.govUK.strokes.listDivider))
                 .ignoresSafeArea()
             SwiftUIButton(
-                .secondary,
-                viewModel: viewModel.goToGovUkButtonViewModel
+                viewModel.error == AppConfigError.networkUnavailable ? .primary : .secondary,
+                viewModel: viewModel.buttonViewModel
             )
-            .accessibilityLabel(viewModel.goToGovUkAccessibilityButtonTitle)
-            .accessibilityHint(viewModel.goToGovUkAccessibilityButtonHint)
+            .accessibilityLabel(viewModel.buttonAccessibilityTitle)
+            .accessibilityHint(viewModel.buttonAccessibilityHint)
             .frame(minHeight: 44, idealHeight: 44)
             .padding([.top, .horizontal], 16)
             .ignoresSafeArea()
+            .disabled(viewModel.showProgressView)
         }
         .navigationBarHidden(true)
     }
-}
-
-#Preview {
-    let viewModel = AppUnavailableContainerViewModel()
-    return AppUnavailableContainerView(viewModel: viewModel)
 }
--- Production/govuk_ios/ViewModels/AppUnavailableContainerViewModel.swift
+++ Production/govuk_ios/ViewModels/AppUnavailableContainerViewModel.swift
@@ -5,28 +5,62 @@ import GOVKit
 
 class AppUnavailableContainerViewModel: ObservableObject {
     private let urlOpener: URLOpener
+    private let appLaunchService: AppLaunchServiceInterface
+    let error: AppConfigError?
+    private let dismissAction: () -> Void
+    @Published var showProgressView: Bool = false
 
-    let title = String.appAvailability.localized("unavailableTitle")
-    let subheading = String.appAvailability.localized("unavailableSubheading")
-    let goToGovUkButtonTitle = String.appAvailability.localized("goToGovUkButtonTitle")
-    let goToGovUkAccessibilityButtonTitle =
+    private(set) var title = String.appAvailability.localized("unavailableTitle")
+    private(set) var subheading = String.appAvailability.localized("unavailableSubheading")
+    private(set) var buttonTitle = String.appAvailability.localized("goToGovUkButtonTitle")
+    private(set) var buttonAccessibilityTitle =
     String.appAvailability.localized("goToGovUkAccessibilityButtonTitle")
-    let goToGovUkAccessibilityButtonHint = String.common.localized("openWebLinkHint")
+    private(set) var buttonAccessibilityHint = String.common.localized("openWebLinkHint")
 
-    var goToGovUkButtonViewModel: GOVUKButton.ButtonViewModel {
+    var buttonViewModel: GOVUKButton.ButtonViewModel {
         .init(
-            localisedTitle: goToGovUkButtonTitle,
+            localisedTitle: buttonTitle,
             action: { [weak self] in
-                self?.openGovUK()
+                if self?.error == .networkUnavailable {
+                    self?.retry()
+                } else {
+                    self?.openGovUK()
+                }
             }
         )
     }
 
-    init(urlOpener: URLOpener = UIApplication.shared) {
+    init(urlOpener: URLOpener = UIApplication.shared,
+         appLaunchService: AppLaunchServiceInterface,
+         error: AppConfigError? = nil,
+         dismissAction: @escaping () -> Void) {
         self.urlOpener = urlOpener
+        self.appLaunchService = appLaunchService
+        self.error = error
+        self.dismissAction = dismissAction
+        handleNetworkError()
+    }
+
+    private func handleNetworkError() {
+        guard self.error == .networkUnavailable else { return }
+        title = String.common.localized("networkUnavailableErrorTitle")
+        subheading = String.common.localized("networkUnavailableErrorBody")
+        buttonTitle = String.common.localized("networkUnavailableButtonTitle")
+        buttonAccessibilityTitle = String.common.localized("networkUnavailableButtonTitle")
+        buttonAccessibilityHint = ""
     }
 
     private func openGovUK() {
         urlOpener.openIfPossible(Constants.API.govukBaseUrl)
+    }
+
+    private func retry() {
+        showProgressView = true
+        appLaunchService.fetch { [weak self] result in
+            if case .success = result.configResult {
+                self?.dismissAction()
+            }
+            self?.showProgressView = false
+        }
     }
 }

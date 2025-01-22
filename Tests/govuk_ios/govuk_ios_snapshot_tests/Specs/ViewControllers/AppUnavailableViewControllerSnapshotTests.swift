5b1de595afb58543287315f8b189318e4e3868c1
Joshua Dubey <jodubey@deloitte.co.uk>
2025-01-22 14:00:02 +0000

    Update snapshot tests

--- Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/AppUnavailableViewControllerSnapshotTests.swift
+++ Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/AppUnavailableViewControllerSnapshotTests.swift
@@ -6,10 +6,11 @@ import SwiftUI
 
 @MainActor
 final class AppUnavailableViewControllerSnapshotTests: SnapshotTestCase {
-    private var view: some View {
+    private func view(error: AppConfigError? = nil) -> some View {
         return AppUnavailableContainerView(
             viewModel: AppUnavailableContainerViewModel(
                 appLaunchService: MockAppLaunchService(),
+                error: error,
                 dismissAction: { }
             )
         )
@@ -17,7 +18,7 @@ final class AppUnavailableViewControllerSnapshotTests: SnapshotTestCase {
 
     func test_loadInNavigationController_light_rendersCorrectly() {
         VerifySnapshotInNavigationController(
-            view: view,
+            view: view(),
             mode: .light,
             navBarHidden: true
         )
@@ -25,7 +26,23 @@ final class AppUnavailableViewControllerSnapshotTests: SnapshotTestCase {
 
     func test_loadInNavigationController_dark_rendersCorrectly() {
         VerifySnapshotInNavigationController(
-            view: view,
+            view: view(),
+            mode: .dark,
+            navBarHidden: true
+        )
+    }
+
+    func test_loadInNavigationController_networkError_light_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            view: view(error: .networkUnavailable),
+            mode: .light,
+            navBarHidden: true
+        )
+    }
+
+    func test_loadInNavigationController_networkError_dark_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            view: view(error: .networkUnavailable),
             mode: .dark,
             navBarHidden: true
         )

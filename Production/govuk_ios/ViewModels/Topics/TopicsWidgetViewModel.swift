0ede4a4973a01c807d4b72e3bb892da06a91e207
joshdubey <95635393+joshdubey@users.noreply.github.com>
2025-01-28 13:57:09 +0000

    post amigos fixes 28 jan (#248)
    * Update feedback stat text to match button title
    
    * Hide "Show all topics" button if there is a fetch error
    
    * Fix code smell

--- Production/govuk_ios/ViewModels/Topics/TopicsWidgetViewModel.swift
+++ Production/govuk_ios/ViewModels/Topics/TopicsWidgetViewModel.swift
@@ -25,7 +25,8 @@ final class TopicsWidgetViewModel {
     }
 
     var allTopicsButtonHidden: Bool {
-        displayedTopics.count >= topicsService.fetchAll().count
+        (displayedTopics.count >= topicsService.fetchAll().count) ||
+        fetchTopicsError
     }
 
     var widgetTitle: String {

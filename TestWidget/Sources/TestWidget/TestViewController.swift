7b88940ee5e0a87333162e2b610057e4643e8357
Joshua Dubey <jodubey@deloitte.co.uk>
2025-02-19 13:23:19 +0000

    Demonstrate fetching of RecentActivities from shared Core Data repo

--- TestWidget/Sources/TestWidget/TestViewController.swift
+++ TestWidget/Sources/TestWidget/TestViewController.swift
@@ -10,6 +10,7 @@ class TestViewController: UIViewController,
     var analyticsService: (any AnalyticsServiceInterface)?
 
     private let coreData: CoreDataRepository
+    private let activityService: ActivityServiceInterface
 
     private let stackView: UIStackView = {
         let stackView = UIStackView()
@@ -33,9 +34,32 @@ class TestViewController: UIViewController,
         return label
     }()
 
-    private lazy var actionButtonViewModel: GOVUKButton.ButtonViewModel = {
+    private lazy var activityLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.govUK.bodySemibold
+        label.textColor = UIColor.govUK.text.primary
+        label.lineBreakMode = .byTruncatingTail
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
+        label.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        label.text = "No activities"
+        return label
+    }()
+
+    private lazy var activityButtonViewModel: GOVUKButton.ButtonViewModel = {
         .init(
-            localisedTitle: "Click me",
+            localisedTitle: "Fetch activity",
+            action: {
+                let activities = self.activityService.fetch().fetchedObjects
+                self.activityLabel.text = activities?.first?.title
+            }
+        )
+    }()
+
+    private lazy var saveDateButtonViewModel: GOVUKButton.ButtonViewModel = {
+        .init(
+            localisedTitle: "Save Date",
             action: {
                 let item = WidgetItem(context: self.coreData.viewContext)
                 item.name = Date().formatted()
@@ -43,7 +67,7 @@ class TestViewController: UIViewController,
                 self.titleLabel.text = item.name
                 _ = try! self.coreData.viewContext.save()
                 self.analyticsService?.track(event: .buttonFunction(
-                    text: "Click me",
+                    text: "Save Date",
                     section: "test",
                     action: "tap")
                 )
@@ -51,15 +75,23 @@ class TestViewController: UIViewController,
         )
     }()
 
-    private lazy var actionButton: GOVUKButton = {
+    private lazy var saveDateButton: GOVUKButton = {
         let button = GOVUKButton(
             .secondary,
-            viewModel: actionButtonViewModel)
+            viewModel: saveDateButtonViewModel)
+        return button
+    }()
+
+    private lazy var activityButton: GOVUKButton = {
+        let button = GOVUKButton(
+            .primary,
+            viewModel: activityButtonViewModel)
         return button
     }()
 
     init() {
         coreData = TestWidgetRepository().coreData
+        activityService = ActivityService(repository: Container.shared.activityRepository.resolve())
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -70,8 +102,10 @@ class TestViewController: UIViewController,
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Test"
+        stackView.addArrangedSubview(activityLabel)
         stackView.addArrangedSubview(titleLabel)
-        stackView.addArrangedSubview(actionButton)
+        stackView.addArrangedSubview(activityButton)
+        stackView.addArrangedSubview(saveDateButton)
         view.addSubview(stackView)
         navigationItem.largeTitleDisplayMode = .never
         navigationItem.backButtonTitle = "Test"
@@ -79,8 +113,8 @@ class TestViewController: UIViewController,
         self.view.backgroundColor = UIColor.govUK.fills.surfaceBackground
         NSLayoutConstraint.activate([
             stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
             stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
         self.analyticsService?.track(screen: self)

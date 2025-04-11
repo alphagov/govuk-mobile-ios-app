408df7639927903a3b585d9d6e0daa67744ba612
Naveed <naveed.ahmad@digital.cabinet-office.gov.uk>
2025-04-11 14:27:21 +0100

    Remove debugging

--- Production/govuk_ios/Deeplinking/DeeplinkRoutes/WebDeeplinkRoute.swift
+++ Production/govuk_ios/Deeplinking/DeeplinkRoutes/WebDeeplinkRoute.swift
@@ -14,24 +14,9 @@ struct WebDeeplinkRoute: DeeplinkRoute {
 
     @MainActor
     func action(parent: BaseCoordinator, params: [String: String]) {
-        // The target URL should be in the params
-        if let targetUrl = params["url"] {
-            print("Raw URL string: \(targetUrl)")
-
-            // Properly decode the URL string before creating the URL object
-            let decodedUrlString = targetUrl.removingPercentEncoding ?? targetUrl
-
-            if let url = URL(string: decodedUrlString) {
-                print("Decoded URL string: \(decodedUrlString)")
-                presentDeeplinkPage(for: url, coordinator: parent)
-            } else {
-                // Fallback if URL creation fails
-                print("Failed to create URL from string: \(targetUrl)")
-            }
-        } else {
-            print("No 'url' parameter found in params: \(params)")
+        if let targetUrl = params["url"], let url = URL(string: targetUrl) {
+            presentDeeplinkPage(for: url, coordinator: parent)
         }
-        print("All params received: \(params)")
     }
 
     @MainActor

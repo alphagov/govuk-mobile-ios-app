129b29789a5fdca01cb95934f2aed532689551cc
raphael olumofe <raphaelo@hotmail.co.uk>
2025-04-13 22:53:10 +0100

    Gov request tests

---
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Networking/GOVRequest/GOVRequest+LocalAuthorityTests.swift
@@ -0,0 +1,24 @@
+import Testing
+import GOVKit
+import Foundation
+
+@testable import govuk_ios
+
+@Suite
+struct GOVRequest_LocalAuthorityTests {
+
+    @Test
+    func local_returnsExpectedVlues() async throws {
+        let expectedPostcode = "SW1A 0AA"
+        let request = GOVRequest.local(postcode: expectedPostcode)
+
+        #expect(request.urlPath == "/find-local-council/query.json")
+        #expect(request.method == .get)
+        #expect(request.signingKey == nil)
+        #expect(request.bodyParameters == nil)
+        #expect(request.queryParameters?.count == 1)
+        #expect(request.queryParameters?["postcode"] == expectedPostcode)
+        #expect(request.additionalHeaders == nil)
+
+    }
+}

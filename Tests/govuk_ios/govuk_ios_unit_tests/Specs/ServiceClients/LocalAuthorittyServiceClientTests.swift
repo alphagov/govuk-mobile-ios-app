d6840a73c63bb87a2225818322f0a1ad1e55faa7
raphael olumofe <raphaelo@hotmail.co.uk>
2025-04-13 22:51:11 +0100

    added unit tests

---
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/ServiceClients/LocalAuthorittyServiceClientTests.swift
@@ -0,0 +1,205 @@
+import Foundation
+import Testing
+
+@testable import govuk_ios
+
+struct LocalAuthorityServiceClientTests {
+
+    @Test
+    func fetchLocal_sendsExpectedResults() async throws {
+
+        let mockAPI = MockAPIServiceClient()
+        let sut = LocalAuthorityServiceClient(serviceClient: mockAPI)
+
+        let expectedPostcode = "SE129PT"
+        sut.fetchLocal(
+            postcode: expectedPostcode) { _ in }
+
+        #expect(mockAPI._receivedSendRequest?.urlPath == "/find-local-council/query.json")
+        #expect(mockAPI._receivedSendRequest?.method == .get)
+        #expect(mockAPI._receivedSendRequest?.queryParameters?["postcode"] as? String == expectedPostcode)
+    }
+
+    @Test
+    func fetchLocal_success_addressLists_returnsExpectedResult() async throws {
+        let mockAPI = MockAPIServiceClient()
+
+        let sut = LocalAuthorityServiceClient(
+            serviceClient: mockAPI
+        )
+        let addresses:[LocalAuthorityAddress] = [
+            LocalAuthorityAddress(
+                address: "address1",
+                slug: "slug1",
+                name: "name1"
+            ),
+            LocalAuthorityAddress(
+                address: "address2",
+                slug: "slug2",
+                name: "name2"
+            )
+
+        ]
+        let expectedResult = LocalAuthoritiesList(addresses: addresses)
+
+        let stubbedData = try! JSONEncoder().encode(expectedResult)
+        mockAPI._stubbedSendResponse = .success(stubbedData)
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(
+                postcode: "test") { result in
+                    continuation.resume(returning: result)
+                }
+        }
+        let localResult = try? result.get()
+        let addressList = localResult as? LocalAuthoritiesList
+        #expect(addressList?.addresses.count == 2)
+        #expect(addressList?.addresses.first?.name == "name1")
+        #expect(addressList?.addresses[1].name == "name2")
+        #expect(addressList?.addresses.first?.slug == "slug1")
+        #expect(addressList?.addresses[1].name == "name2")
+    }
+
+    @Test
+    func fetchLocal_success_tierOnelocalAuthority_returnsExpectedResult() async throws {
+        let mockAPI = MockAPIServiceClient()
+
+        let sut = LocalAuthorityServiceClient(
+            serviceClient: mockAPI
+        )
+        let authority = Authority(
+            name: "name1",
+            homepageUrl: "homepageUrl",
+            tier: "tier1",
+            slug: "slug1",
+            parent: nil
+        )
+
+        let expectedResult = LocalAuthority(localAuthority: authority)
+
+        let stubbedData = try! JSONEncoder().encode(expectedResult)
+        mockAPI._stubbedSendResponse = .success(stubbedData)
+
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(postcode: "test") { result in
+                continuation.resume(returning: result)
+            }
+        }
+        let localResult = try? result.get()
+        let localAuthority = localResult as? LocalAuthority
+        #expect(localAuthority?.localAuthority.homepageUrl == "homepageUrl")
+        #expect(localAuthority?.localAuthority.name == "name1")
+        #expect(localAuthority?.localAuthority.tier == "tier1")
+        #expect(localAuthority?.localAuthority.slug == "slug1")
+    }
+
+    @Test
+    func fetchLocal_success_tierTwolocalAuthority_returnsExpectedResult() async throws {
+        let mockAPI = MockAPIServiceClient()
+        let sut = LocalAuthorityServiceClient(
+            serviceClient: mockAPI
+        )
+
+        let parentAuthority = Authority(
+            name: "parentAuthority",
+            homepageUrl: "homepageUrl",
+            tier: "parentTier",
+            slug: "slug",
+            parent: nil
+        )
+
+        let authority = Authority(
+            name: "name2",
+            homepageUrl: "homepageUrl",
+            tier: "tier2",
+            slug: "slug2",
+            parent: parentAuthority
+        )
+
+        let expectedResult = LocalAuthority(localAuthority: authority)
+
+        let stubbedData = try! JSONEncoder().encode(expectedResult)
+        mockAPI._stubbedSendResponse = .success(stubbedData)
+
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(postcode: "test") { result in
+                continuation.resume(returning: result)
+            }
+        }
+        let localResult = try? result.get()
+        let localAuthority = localResult as? LocalAuthority
+        #expect(localAuthority?.localAuthority.homepageUrl == "homepageUrl")
+        #expect(localAuthority?.localAuthority.name == "name2")
+        #expect(localAuthority?.localAuthority.tier == "tier2")
+        #expect(localAuthority?.localAuthority.slug == "slug2")
+        #expect(localAuthority?.localAuthority.parent?.name == "parentAuthority")
+    }
+
+    @Test
+    func fetchLocal_failure_apiUnavailable_returnsExpectedResult() async {
+        let mockAPI = MockAPIServiceClient()
+        let sut = LocalAuthorityServiceClient(serviceClient: mockAPI)
+
+        mockAPI._stubbedSendResponse = .failure(TestError.fakeNetwork)
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(postcode: "test") { result in
+                continuation.resume(returning: result)
+            }
+        }
+        let localResult = try? result.get()
+        #expect(localResult == nil)
+        #expect(result.getError() == .apiUnavailable)
+    }
+
+    @Test
+    func fetchLocal_failure_networkUnavailable_returnsExpectedResult() async {
+        let mockAPI = MockAPIServiceClient()
+        let sut = LocalAuthorityServiceClient(
+            serviceClient: mockAPI
+        )
+        mockAPI._stubbedSendResponse = .failure(
+            NSError(domain: "TestError", code: NSURLErrorNotConnectedToInternet)
+        )
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(postcode: "test") { result in
+                continuation.resume(returning: result)
+            }
+        }
+        let localResult = try? result.get()
+        #expect(localResult == nil)
+        #expect(result.getError() == .networkUnavailable)
+    }
+
+    @Test
+    func fetchLocal_decodingError_returnsExpectedResult() async {
+        let mockAPI = MockAPIServiceClient()
+        let sut = LocalAuthorityServiceClient(serviceClient: mockAPI)
+        let invalidObject = try! JSONEncoder().encode("Test")
+        mockAPI._stubbedSendResponse = .success(invalidObject)
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(postcode: "test") { result in
+                continuation.resume(returning: result)
+            }
+        }
+        let localResult = try? result.get()
+        #expect(localResult == nil)
+        #expect(result.getError() == .decodingError)
+    }
+
+    @Test
+    func fetchLocal_error_returnsExpectedresult() async throws {
+        let mockAPI = MockAPIServiceClient()
+        let sut = LocalAuthorityServiceClient(serviceClient: mockAPI)
+
+        let stubbedData = try! JSONEncoder().encode(LocalErrorMessage(message: "errorMessage"))
+        mockAPI._stubbedSendResponse = .success(stubbedData)
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(
+                postcode: "test") { result in
+                    continuation.resume(returning: result)
+                }
+        }
+        let localResult = try? result.get()
+        let errormessage = localResult as? LocalErrorMessage
+        #expect(errormessage?.message == "")
+    }
+}
---
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Services/LocalAuthortiyService.swift
@@ -0,0 +1,187 @@
+import Foundation
+import Testing
+
+@testable import govuk_ios
+
+@Suite
+struct LocalAuthorityServiceTests {
+
+    @Test
+    func fetchLocal_callsServiceClient() async throws {
+        let mockServiceClient = MockLocalServiceClient()
+        let sut = LocalAuthorityService(serviceClient: mockServiceClient)
+        let expectedPostcode = "SW1"
+
+        sut.fetchLocal(
+            postcode: expectedPostcode) { _ in }
+        #expect(mockServiceClient._stubbedPostcode == expectedPostcode)
+    }
+
+    @Test
+    func fetchLocal_success_addressLists_returnsExpectedResult() async throws {
+        let addresses:[LocalAuthorityAddress] = [
+            LocalAuthorityAddress(
+                address: "address1",
+                slug: "slug1",
+                name: "name1"
+            ),
+            LocalAuthorityAddress(
+                address: "address2",
+                slug: "slug2",
+                name: "name2"
+            )
+
+        ]
+        let expectedResult = LocalAuthoritiesList(addresses: addresses)
+        let mockServiceClient = MockLocalServiceClient()
+        mockServiceClient._stubbedLocalResult = .success(expectedResult)
+        let sut = LocalAuthorityService(serviceClient: mockServiceClient)
+
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(
+                postcode: "test") { result in
+                    continuation.resume(returning: result)
+                }
+        }
+        let localResult = try? result.get()
+        let addressList = localResult as? LocalAuthoritiesList
+        #expect(addressList?.addresses.count == 2)
+        #expect(addressList?.addresses.first?.name == "name1")
+        #expect(addressList?.addresses[1].name == "name2")
+        #expect(addressList?.addresses.first?.slug == "slug1")
+        #expect(addressList?.addresses[1].name == "name2")
+    }
+
+    @Test
+    func fetchLocal_success_tierOnelocalAuthority_returnsExpectedResult() async throws {
+        let authority = Authority(
+            name: "name1",
+            homepageUrl: "homepageUrl",
+            tier: "tier1",
+            slug: "slug1",
+            parent: nil
+        )
+        let expectedResult = LocalAuthority(localAuthority: authority)
+        let mockServiceClient = MockLocalServiceClient()
+        mockServiceClient._stubbedLocalResult = .success(expectedResult)
+        let sut = LocalAuthorityService(serviceClient: mockServiceClient)
+
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(
+                postcode: "test") { result in
+                    continuation.resume(returning: result)
+                }
+        }
+        let localResult = try? result.get()
+        let localAuthority = localResult as? LocalAuthority
+        #expect(localAuthority?.localAuthority.homepageUrl == "homepageUrl")
+        #expect(localAuthority?.localAuthority.name == "name1")
+        #expect(localAuthority?.localAuthority.tier == "tier1")
+        #expect(localAuthority?.localAuthority.slug == "slug1")
+    }
+
+    @Test
+    func fetchLocal_success_tierTwolocalAuthority_returnsExpectedResult() async throws {
+        let parentAuthority = Authority(
+            name: "parentAuthority",
+            homepageUrl: "homepageUrl",
+            tier: "parentTier",
+            slug: "slug",
+            parent: nil
+        )
+
+        let authority = Authority(
+            name: "name2",
+            homepageUrl: "homepageUrl",
+            tier: "tier2",
+            slug: "slug2",
+            parent: parentAuthority
+        )
+
+        let expectedResult = LocalAuthority(localAuthority: authority)
+        let mockServiceClient = MockLocalServiceClient()
+        mockServiceClient._stubbedLocalResult = .success(expectedResult)
+
+        let sut = LocalAuthorityService(serviceClient: mockServiceClient)
+
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(
+                postcode: "test") { result in
+                    continuation.resume(returning: result)
+                }
+        }
+        let localResult = try? result.get()
+        let localAuthority = localResult as? LocalAuthority
+        #expect(localAuthority?.localAuthority.homepageUrl == "homepageUrl")
+        #expect(localAuthority?.localAuthority.name == "name2")
+        #expect(localAuthority?.localAuthority.tier == "tier2")
+        #expect(localAuthority?.localAuthority.slug == "slug2")
+        #expect(localAuthority?.localAuthority.parent?.name == "parentAuthority")
+    }
+
+    @Test
+    func fetchLocal_apiUnavailable_returnsExpectedResult() async throws {
+        let mockServiceClient = MockLocalServiceClient()
+        mockServiceClient._stubbedLocalResult = .failure(.apiUnavailable)
+        let sut = LocalAuthorityService(serviceClient: mockServiceClient)
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(
+                postcode: "test") { result in
+                    continuation.resume(returning: result)
+                }
+        }
+        let localResult = try? result.get()
+        #expect(localResult == nil)
+        #expect(result.getError() == .apiUnavailable)
+    }
+
+
+    @Test
+    func fetchLocal_decodingError_returnsExpectedResult() async throws {
+        let mockServiceClient = MockLocalServiceClient()
+        mockServiceClient._stubbedLocalResult = .failure(.decodingError)
+        let sut = LocalAuthorityService(serviceClient: mockServiceClient)
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(
+                postcode: "test") { result in
+                    continuation.resume(returning: result)
+                }
+        }
+        let localResult = try? result.get()
+        #expect(localResult == nil)
+        #expect(result.getError() == .decodingError)
+    }
+
+    @Test
+    func fetchLocal_networkUnavailable_returnsExpectedResult() async throws {
+        let mockServiceClient = MockLocalServiceClient()
+        mockServiceClient._stubbedLocalResult = .failure(.networkUnavailable)
+        let sut = LocalAuthorityService(serviceClient: mockServiceClient)
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(
+                postcode: "test") { result in
+                    continuation.resume(returning: result)
+                }
+        }
+        let localResult = try? result.get()
+        #expect(localResult == nil)
+        #expect(result.getError() == .networkUnavailable)
+    }
+
+    @Test
+    func fetchLocal_errorCase_returnsExpectedResult() async throws {
+        let mockServiceClient = MockLocalServiceClient()
+        mockServiceClient._stubbedLocalResult = .success(LocalErrorMessage(message: "errorMessage"))
+
+        let sut = LocalAuthorityService(serviceClient: mockServiceClient)
+        let result = await withCheckedContinuation { continuation in
+            sut.fetchLocal(
+                postcode: "test") { result in
+                    continuation.resume(returning: result)
+                }
+        }
+        let localResult = try? result.get()
+        let errorMessage = localResult as? LocalErrorMessage
+        #expect(errorMessage?.message == "errorMessage")
+    }
+}

2bbfc9d8f1883c17bb182a4b4bd1536567b3bfb4
Tom Bates <batesyuk@users.noreply.github.com>
2025-04-17 12:45:04 +0100

    Updated UIComponents & List Divider (#342)
    * Updated list divider color setting
    
    * Updated UIComponents
    
    * Removed TopicsButtonView
    
    ---------
    
    Co-authored-by: Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>

--- Production/govuk_ios/SwiftUIViews/AppUnavailableContainerView.swift
+++ Production/govuk_ios/SwiftUIViews/AppUnavailableContainerView.swift
@@ -25,7 +25,7 @@ struct AppUnavailableContainerView: View {
             .padding(.top, 1)
             Spacer()
             Divider()
-                .foregroundColor(Color(UIColor.govUK.strokes.listDivider))
+                .overlay(Color(UIColor.govUK.strokes.listDivider))
                 .ignoresSafeArea()
             SwiftUIButton(
                 viewModel.error == AppConfigError.networkUnavailable ? .primary : .secondary,

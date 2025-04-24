b13e142d43cada6e06d6ee95895bbae922175a1f
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2025-04-24 12:09:24 +0100

    Update local onboarding view
    Mimics onboarding package slide.

--- Production/govuk_ios/Views/LocalAuthentication/LocalAuthenticationOnboardingView.swift
+++ Production/govuk_ios/Views/LocalAuthentication/LocalAuthenticationOnboardingView.swift
@@ -10,43 +10,55 @@ struct LocalAuthenticationOnboardingView: View {
     }
 
     var body: some View {
-        VStack(spacing: 24) {
+        VStack {
+            if verticalSizeClass == .regular {
+                Spacer(minLength: 32)
+            }
+            let imageTopPadding = verticalSizeClass == .compact ? CGFloat(16) : CGFloat(64)
             ScrollView {
                 Image(systemName: viewModel.iconName)
                     .resizable()
                     .frame(width: 64, height: 64)
-                    .fontWeight(.light)
+                    .fontWeight(.thin)
+                    .padding(.top, imageTopPadding)
+                    .padding(.horizontal, 16)
                 Text(viewModel.title)
                     .multilineTextAlignment(.center)
                     .font(Font.govUK.largeTitleBold)
-                    .padding(.bottom, 20)
+                    .padding(.bottom, 16)
+                    .accessibilityAddTraits(.isHeader)
+                    .accessibilitySortPriority(1)
                 Text(viewModel.message)
                     .font(Font.govUK.body)
                     .multilineTextAlignment(.center)
-                    .padding(.horizontal, 20)
+                    .padding(.horizontal, 16)
+                    .accessibilitySortPriority(0)
                 Spacer()
             }
-            Divider()
-                .foregroundColor(Color(UIColor.govUK.strokes.listDivider))
-                .ignoresSafeArea()
+            .accessibilityElement(children: .contain)
             let buttonLayout = verticalSizeClass == .compact ?
             AnyLayout(HStackLayout()) :
             AnyLayout(VStackLayout())
-            buttonLayout {
-                SwiftUIButton(
-                    .primary,
-                    viewModel: viewModel.enrolButtonViewModel
-                )
-                .frame(minHeight: 44, idealHeight: 44)
-                .padding(.horizontal, 15)
-                SwiftUIButton(
-                    .secondary,
-                    viewModel: viewModel.skipButtonViewModel
-                )
-                .frame(minHeight: 44, idealHeight: 44)
-                .padding(.horizontal, 15)
-            }.padding(.top, 16)
-        }.padding(.top, 100)
-        .navigationBarBackButtonHidden()
+            VStack(alignment: .center, spacing: 16) {
+                Divider()
+                    .background(Color(UIColor.govUK.strokes.listDivider))
+                    .ignoresSafeArea(edges: [.leading, .trailing])
+                    .padding([.top], 0)
+                buttonLayout {
+                    SwiftUIButton(
+                        .primary,
+                        viewModel: viewModel.enrolButtonViewModel
+                    )
+                    .frame(minHeight: 44, idealHeight: 44)
+                    SwiftUIButton(
+                        .secondary,
+                        viewModel: viewModel.skipButtonViewModel
+                    )
+                    .frame(minHeight: 44, idealHeight: 44)
+                }
+            }
+            .padding([.leading, .trailing], verticalSizeClass == .regular ? 16 : 0)
+            .navigationBarBackButtonHidden()
+        }.navigationBarHidden(true)
     }
 }

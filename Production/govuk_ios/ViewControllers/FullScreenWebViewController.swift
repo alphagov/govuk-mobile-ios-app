38066b11471180071ac55894f3508e707b9ca385
Naveed <naveed.ahmad@digital.cabinet-office.gov.uk>
2025-04-30 10:26:59 +0100

    Inject html contents

--- Production/govuk_ios/ViewControllers/FullScreenWebViewController.swift
+++ Production/govuk_ios/ViewControllers/FullScreenWebViewController.swift
@@ -6,25 +6,54 @@ class FullScreenWebViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "WebView Test"
         setupWebView()
+        setupCloseButton()
+        loadContent()
     }
 
     private func setupWebView() {
-        // Create WKWebView configuration
         let configuration = WKWebViewConfiguration()
         webView = WKWebView(frame: view.bounds, configuration: configuration)
         webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(webView)
-
-        // Load Google.com as a test
-        if let url = URL(string: "https://google.com") {
-            let request = URLRequest(url: url)
-            webView.load(request)
-        }
     }
 
-    // Method to load HTML content
-    func loadHTML(_ htmlString: String) {
+    private func setupCloseButton() {
+        let closeButton = UIBarButtonItem(barButtonSystemItem: .done,
+                                          target: self, action: #selector(dismissWebView))
+        navigationItem.rightBarButtonItem = closeButton
+    }
+
+    private func loadContent() {
+        let htmlString = """
+        <p>Sign in to your Universal Credit account to:</p>
+
+        <ul>
+          <li>apply for an advance on your first payment</li>
+          <li>see your statement</li>
+          <li>report a change in circumstances</li>
+          <li>add a note to your journal</li>
+          <li>see your to-do list</li>
+          <li>see when your next payment will be</li>
+          <li>see your Claimant Commitment</li>
+        </ul>
+
+        <p>Use the username and password you set up when you applied for
+        Universal Credit. You can ask for a reminder if you're not sure.
+        </p>
+
+        <div role="note" aria-label="Information" class="application-notice info-notice">
+            <p>This service is also available
+                <a href="/mewngofnodi-ich-cyfrif-credyd-cynhwysol">in Welsh (Cymraeg)</a>.
+            </p>
+        </div>
+        """
         webView.loadHTMLString(htmlString, baseURL: nil)
+    }
+
+    @objc private func dismissWebView() {
+        dismiss(animated: true, completion: nil)
     }
 }

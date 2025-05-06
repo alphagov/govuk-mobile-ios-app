edcd976fd17a4028324c4c4bb9b2d9c900c09371
raphael olumofe <raphaelo@hotmail.co.uk>
2025-05-06 14:52:45 +0100

    removed not needed extensions

--- Production/govuk_ios/Extensions/SwiftUI/TextFieldStyle+Extensions.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import SwiftUI
-
-extension TextFieldStyle where Self == CustomTextFieldBorder {
-    static var customTextFieldBorder: CustomTextFieldBorder { CustomTextFieldBorder() }
-}

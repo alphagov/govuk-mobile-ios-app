23eb59fabd398f4ed1cef41b310f9eb9c5b6d086
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2025-05-14 11:13:08 +0100

    Added NotificationsOnboardingViewSnapshotTests

---
+++ Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/NotificationsOnboardingViewSnapshotTests.swift
@@ -0,0 +1,49 @@
+import Foundation
+import XCTest
+import GOVKit
+
+@testable import GOVKitTestUtilities
+@testable import govuk_ios
+
+@MainActor
+class NotificationsOnboardingViewSnapshotTests: SnapshotTestCase {
+
+    override func setUp() {
+        super.setUp()
+        UIView.setAnimationsEnabled(false)
+    }
+
+    func test_loadInNavigationController_light_rendersCorrectly() {
+        let viewModel = NotificationsOnboardingViewModel(
+            urlOpener: MockURLOpener(),
+            analyticsService: MockAnalyticsService(),
+            completeAction: { },
+            dismissAction: { }
+        )
+        let view = NotificationsOnboardingView(viewModel: viewModel)
+        let viewController = HostingViewController(rootView: view)
+
+        VerifySnapshotInNavigationController(
+            viewController: viewController,
+            mode: .light,
+            navBarHidden: true,
+        )
+    }
+
+    func test_loadInNavigationController_dark_rendersCorrectly() {
+        let viewModel = NotificationsOnboardingViewModel(
+            urlOpener: MockURLOpener(),
+            analyticsService: MockAnalyticsService(),
+            completeAction: { },
+            dismissAction: { }
+        )
+        let view = NotificationsOnboardingView(viewModel: viewModel)
+        let viewController = HostingViewController(rootView: view)
+
+        VerifySnapshotInNavigationController(
+            viewController: viewController,
+            mode: .dark,
+            navBarHidden: true,
+        )
+    }
+}

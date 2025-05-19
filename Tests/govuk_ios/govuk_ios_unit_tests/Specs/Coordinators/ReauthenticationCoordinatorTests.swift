40986fc2aefac6d87de1cdc437cc5911f6c1b89c
Joshua Dubey <jodubey@deloitte.co.uk>
2025-05-19 14:16:59 +0100

    Fix test

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/Coordinators/ReauthenticationCoordinatorTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Coordinators/ReauthenticationCoordinatorTests.swift
@@ -68,11 +68,11 @@ class ReauthenticationCoordinatorTests {
         let mockAuthenticationService = MockAuthenticationService()
         let mockNavigationController =  MockNavigationController()
         let mockLocalAuthenticationService = MockLocalAuthenticationService()
+        mockLocalAuthenticationService._stubbedAuthenticationOnboardingSeen = true
         mockLocalAuthenticationService._stubbedBiometricsHaveChanged = true
         mockCoordinatorBuilder._stubbedAuthenticationOnboardingCoordinator =
         mockAuthenticationOnboardingCoordinator
         mockAuthenticationService._stubbedTokenRefreshRequest = .failure(.genericError)
-        // Need too add continuation as starting coordinator is called after #expect
         let authenticationOnboardingStartCalled = await withCheckedContinuation { continuation in
             mockAuthenticationOnboardingCoordinator._startCalledContinuation = continuation
             let sut = ReauthenticationCoordinator(
@@ -80,7 +80,8 @@ class ReauthenticationCoordinatorTests {
                 coordinatorBuilder: mockCoordinatorBuilder,
                 authenticationService: mockAuthenticationService,
                 localAuthenticationService: mockLocalAuthenticationService,
-                completionAction: { continuation.resume(returning: true) }
+                completionAction: { continuation.resume(returning: true) },
+                newUserAction: { }
             )
             sut.start(url: nil)
         }

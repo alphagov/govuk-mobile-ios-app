6b0f2ab791e4b056eb94fd19f0b142be29be67cc
raphael olumofe <raphaelo@hotmail.co.uk>
2025-05-22 15:32:15 +0100

    real api integration

--- Production/govuk_ios/Model/LocalAuthority/LocalAuthorityAddress.swift
+++ Production/govuk_ios/Model/LocalAuthority/LocalAuthorityAddress.swift
@@ -4,4 +4,10 @@ struct LocalAuthorityAddress: Codable {
     let address: String
     let slug: String
     let name: String
+
+    enum CodingKeys: String, CodingKey {
+        case address = "address"
+        case name = "local_authority_name"
+        case slug = "local_authority_slug"
+    }
 }

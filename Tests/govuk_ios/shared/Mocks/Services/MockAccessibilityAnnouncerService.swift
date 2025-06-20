3123d759980c00b1e0d9be2369a759c3be0fd384
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2025-06-20 14:48:30 +0100

    Commented tests

--- Tests/govuk_ios/shared/Mocks/Services/MockAccessibilityAnnouncerService.swift
+++ Tests/govuk_ios/shared/Mocks/Services/MockAccessibilityAnnouncerService.swift
@@ -3,7 +3,6 @@ import Foundation
 @testable import govuk_ios
 
 class MockAccessibilityAnnouncerService: AccessibilityAnnouncerServiceInterface {
-
     var _receivedAnnounceValue: String?
     func announce(_ value: String) {
         _receivedAnnounceValue = value

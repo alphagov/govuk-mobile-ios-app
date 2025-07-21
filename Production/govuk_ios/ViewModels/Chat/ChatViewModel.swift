8176bc2f8edc9fb8ea0356f7e63d5588529f0f23
Joshua Dubey <jodubey@deloitte.co.uk>
2025-07-21 11:57:28 +0100

    Refactor to allow for separate response to asking question, so we can show "Message loading..." in chat

--- Production/govuk_ios/ViewModels/Chat/ChatViewModel.swift
+++ Production/govuk_ios/ViewModels/Chat/ChatViewModel.swift
@@ -6,7 +6,6 @@ class ChatViewModel: ObservableObject {
     private let chatService: ChatServiceInterface
     private let analyticsService: AnalyticsServiceInterface
 
-    @Published private(set) var questionInProgress: Bool = false
     @Published var cellModels: [ChatCellViewModel] = []
     @Published var latestQuestion: String = ""
     @Published var scrollToBottom: Bool = false
@@ -19,29 +18,37 @@ class ChatViewModel: ObservableObject {
     }
 
     func askQuestion() {
-        // show question in chat
-        let questionModel = ChatCellViewModel(message: latestQuestion,
-                                              id: UUID().uuidString,
-                                              type: .question)
-        cellModels.append(questionModel)
-        questionInProgress = true
-        cellModels.append(.placeHolder)
+        cellModels.append(.loadingQuestion)
         scrollToBottom = true
         chatService.askQuestion(latestQuestion) { [weak self] result in
-            self?.questionInProgress = false
+            self?.cellModels.removeLast()
+            switch result {
+            case .success(let question):
+                let cellModel = ChatCellViewModel(question: question)
+                self?.cellModels.append(cellModel)
+                self?.answeredQuestionID = question.id
+                self?.pollForAnswer(question)
+            case .failure(let error):
+                let cellModel = ChatCellViewModel(error: error)
+                self?.cellModels.append(cellModel)
+            }
+        }
+        latestQuestion = ""
+    }
+
+    private func pollForAnswer(_ question: PendingQuestion) {
+        cellModels.append(.gettingAnswer)
+        chatService.pollForAnswer(question) { [weak self] result in
             self?.cellModels.removeLast()
             switch result {
             case .success(let answer):
-                guard answer.message != nil else { return }
                 let cellModel = ChatCellViewModel(answer: answer)
                 self?.cellModels.append(cellModel)
             case .failure(let error):
                 let cellModel = ChatCellViewModel(error: error)
                 self?.cellModels.append(cellModel)
             }
-            self?.answeredQuestionID = questionModel.id
         }
-        latestQuestion = ""
     }
 
     func loadHistory() {

664b9ae1d3b5c088e1de7c354427abc7d94de3bb
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2025-08-07 10:37:58 +0100

    Make iat optional

--- Production/govuk_ios/Validators/JWT/IdTokenPayload.swift
+++ Production/govuk_ios/Validators/JWT/IdTokenPayload.swift
@@ -3,7 +3,7 @@ import JWTKit
 
 struct IdTokenPayload: JWTPayload {
     let sub: String
-    let iat: Date
+    let iat: Date?
     let email: String
     func verify(using algorithm: some JWTAlgorithm) async throws { /* protocol conformance */ }
 }

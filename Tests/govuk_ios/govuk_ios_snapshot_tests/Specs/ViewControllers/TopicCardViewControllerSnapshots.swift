39f95c0bd06ff152a986d609c346f9a8164dda4e
raphael olumofe <raphaelo@hotmail.co.uk>
2025-08-11 11:34:48 +0100

    topic card snapshots

---
+++ Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/TopicCardViewControllerSnapshots.swift
@@ -0,0 +1,38 @@
+import Foundation
+import XCTest
+import GOVKit
+import UIKit
+
+@testable import GOVKitTestUtilities
+@testable import govuk_ios
+
+@MainActor
+final class TopicCardViewControllerSnapshotTests: SnapshotTestCase {
+    let coreData = CoreDataRepository.arrangeAndLoad
+
+    func test_loadInNavigationController_light_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            viewController: viewController(),
+            mode: .light,
+            prefersLargeTitles: true
+        )
+    }
+
+    func test_loadInNavigationController_dark_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            viewController: viewController(),
+            mode: .dark,
+            prefersLargeTitles: true
+        )
+    }
+
+    private func viewController() -> UIViewController {
+        let context = coreData.viewContext
+        let topic = Topic.arrange(context: context, isFavourite: true)
+        topic.title = "Test topic"
+        let view = TopicCard(model: topic)
+        return HostingViewController(rootView: view)
+    }
+}
+
+

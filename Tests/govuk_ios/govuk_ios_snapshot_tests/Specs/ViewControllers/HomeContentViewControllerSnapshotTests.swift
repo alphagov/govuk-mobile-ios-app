a8e955ac349b2ca0af3734c34fbccf7c17b007cc
raphael olumofe <raphaelo@hotmail.co.uk>
2025-08-12 11:10:14 +0100

    add snapshot tests

---
+++ Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/HomeContentViewControllerSnapshotTests.swift
@@ -0,0 +1,61 @@
+import Foundation
+import XCTest
+import GOVKit
+import UIKit
+import SwiftUI
+
+@testable import GOVKitTestUtilities
+@testable import govuk_ios
+
+@MainActor
+final class HomepageContentViewControllerSnapshotTests: SnapshotTestCase {
+
+    func test_loadInNavigationController_light_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            viewController: viewController(),
+            mode: .light,
+            prefersLargeTitles: true
+        )
+    }
+
+    func test_loadInNavigationController_dark_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            viewController: viewController(),
+            mode: .dark,
+            prefersLargeTitles: true
+        )
+    }
+
+    private func viewController() -> UIViewController {
+        let topicsWidgetViewModel = TopicsWidgetViewModel(
+            topicsService: MockTopicsService(),
+            analyticsService: MockAnalyticsService(),
+            topicAction: {_ in },
+            allTopicsAction: {}
+        )
+        let viewModel = HomeViewModel(
+            analyticsService: MockAnalyticsService(),
+            configService: MockAppConfigService(),
+            notificationService: MockNotificationService(),
+            topicsWidgetViewModel: topicsWidgetViewModel,
+            urlOpener: MockURLOpener(),
+            searchService: MockSearchService(),
+            activityService: MockActivityService(),
+            localAuthorityService: MockLocalAuthorityService(),
+            localAuthorityAction: { },
+            editLocalAuthorityAction: { },
+            feedbackAction: { },
+            notificationsAction: {},
+            recentActivityAction: { } ,
+            openURLAction: {_ in } ,
+            openAction: {_ in }
+        )
+        let view = HomeContentView(
+            viewModel: viewModel
+        )
+        return HostingViewController(rootView: view)
+    }
+}
+
+
+

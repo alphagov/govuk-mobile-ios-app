3186332da45d3a6a119a73926f92571c399b95fb
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2025-08-22 09:57:24 +0100

    Added AlertBanner

---
+++ Production/govuk_ios/Views/Home/AlertBannerWidgetView.swift
@@ -0,0 +1,66 @@
+import Foundation
+import SwiftUI
+
+struct AlertBannerWidgetViewModel {
+    let id: String
+    let body: String
+    let linkUrl: URL?
+    let linkTitle: String?
+    let dismiss: () -> Void
+
+    init(alert: AlertBanner,
+         dismiss: @escaping () -> Void) {
+        self.id = alert.id
+        self.body = alert.body
+        self.linkUrl = alert.link?.url
+        self.linkTitle = alert.link?.title
+        self.dismiss = dismiss
+    }
+
+    func open() {
+        guard let localUrl = linkUrl
+        else { return }
+        UIApplication.shared.open(
+            localUrl,
+            options: [:],
+            completionHandler: nil
+        )
+    }
+}
+
+struct AlertBannerWidgetView: View {
+    let viewModel: AlertBannerWidgetViewModel
+    var body: some View {
+        VStack {
+            HStack {
+                Text(viewModel.body)
+                    .font(.govUK.body)
+                    .foregroundColor(Color(UIColor.govUK.text.primary))
+                Spacer()
+                Image(systemName: "x.circle")
+                    .onTapGesture {
+                        viewModel.dismiss()
+                    }
+            }
+            .accessibilityElement(children: .combine)
+            .accessibilityLabel(viewModel.body)
+            .accessibilityAddTraits(.isLink)
+            .accessibilityHint(String.common.localized("openWebLinkHint"))
+            .padding(.bottom, 4)
+            if let titlelinkTitle = viewModel.linkTitle,
+               !titlelinkTitle.isEmpty {
+                HStack {
+                    Text(titlelinkTitle)
+                        .font(.govUK.body)
+                        .foregroundColor(Color(UIColor.govUK.text.link))
+                        .multilineTextAlignment(.leading)
+                    Spacer()
+                }
+            }
+        }
+        .background(Color(UIColor.govUK.fills.surfaceCardBlue))
+        .onTapGesture {
+            self.viewModel.open()
+        }
+    }
+}

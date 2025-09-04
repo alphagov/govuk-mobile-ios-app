fbdaf185fdea8dfdbcd67f1fcd93c1dfc9d21038
raphael olumofe <raphaelo@hotmail.co.uk>
2025-09-04 21:41:43 +0100

    add scroll view

--- Production/govuk_ios/SwiftUIViews/Chat/ChatUpsellCard.swift
+++ Production/govuk_ios/SwiftUIViews/Chat/ChatUpsellCard.swift
@@ -3,52 +3,61 @@ import UIComponents
 
 struct ChatUpsellCard: View {
     let action: () -> Void
+    @Environment(\.sizeCategory) var sizeCategory
+    @Environment(\.verticalSizeClass) var verticalSizeClass
     var body: some View {
-        VStack {
-            HStack(alignment: .top, spacing: 4) {
-                VStack(alignment: .leading, spacing: 4) {
-                    Text(String.chat.localized("upsellCardTitle"))
-                        .foregroundColor(Color(UIColor.govUK.text.primary))
-                        .font(Font.govUK.bodySemibold)
-                    Text(String.chat.localized("upsellCardDescription"))
-                        .font(Font.govUK.body)
-                        .foregroundColor(Color(UIColor.govUK.text.primary))
-                    Text(String.chat.localized("upsellCardLinkText"))
-                        .font(Font.govUK.body)
-                        .foregroundColor(Color(UIColor.govUK.text.link))
-                }
-                .padding(.trailing, 16)
-                VStack {
-                    Image(decorative: "chat_onboarding_info")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 72, height: 69)
-                }
-                .padding(.vertical, 40)
-                VStack {
-                    Button(action: {
-                        action()
-                    }, label: {
-                        VStack {
-                            Image(systemName: "xmark")
-                                .foregroundColor(Color(UIColor.govUK.text.secondary))
-                        }
-                    })
-                    .accessibilityLabel(
-                        String.chat.localized("upsellCardDismissButtonLabel")
-                    )
-                    .frame(alignment: .trailing)
-                    .padding(.bottom)
-                }
-            }.padding()
+        ScrollView(showsIndicators: sizeCategory > .accessibilityLarge) {
+            VStack {
+                HStack(alignment: .top, spacing: 4) {
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(String.chat.localized("upsellCardTitle"))
+                            .foregroundColor(Color(UIColor.govUK.text.primary))
+                            .font(Font.govUK.bodySemibold)
+                        Text(String.chat.localized("upsellCardDescription"))
+                            .font(Font.govUK.body)
+                            //   .multilineTextAlignment(.leading)
+                            .foregroundColor(Color(UIColor.govUK.text.primary))
+                        Text(String.chat.localized("upsellCardLinkText"))
+                            .font(Font.govUK.body)
+                            .foregroundColor(Color(UIColor.govUK.text.link))
+                    }
+                    .padding(.trailing, 16)
+                    VStack {
+                        Spacer()
+                        Image(decorative: "chat_onboarding_info")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 72, height: 69)
+                        Spacer()
+                    }
+                    .padding(.bottom, returnImagePadding())
+                    VStack {
+                        Button(action: {
+                            action()
+                        }, label: {
+                            VStack {
+                                Image(systemName: "xmark")
+                                    .foregroundColor(Color(UIColor.govUK.text.secondary))
+                            }
+                        })
+                        .accessibilityLabel(
+                            String.chat.localized("upsellCardDismissButtonLabel")
+                        )
+                        .frame(alignment: .trailing)
+                    }
+                }.padding()
+            }
+            .background(Image(decorative: "chat_card_gradient")
+                .resizable()
+            )
+            .roundedBorder(borderColor: .clear)
+            .shadow(color: Color(uiColor: UIColor.govUK.strokes.cardDefault),
+                    radius: 0, x: 0, y: 3
+            )
+            .padding()
         }
-        .background(Image("chat_card_gradient")
-            .resizable()
-        )
-        .roundedBorder(borderColor: .clear)
-        .shadow(color: Color(uiColor: UIColor.govUK.strokes.cardDefault),
-                radius: 0, x: 0, y: 3
-        )
-        .padding()
+    }
+    private func returnImagePadding() -> CGFloat {
+        return sizeCategory < .accessibilityLarge && verticalSizeClass == .regular ? 44 : 0
     }
 }

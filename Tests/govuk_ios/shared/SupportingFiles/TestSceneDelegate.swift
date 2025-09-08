833f97a9320188cdb2d1bd8bad0dc8c6075ff06a
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2025-09-08 09:15:17 +0100

    Added TestDelegate

---
+++ Tests/govuk_ios/shared/SupportingFiles/TestSceneDelegate.swift
@@ -0,0 +1,15 @@
+import Foundation
+import UIKit
+
+class TestSceneDelegate: UIResponder,
+                         UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene)
+        else { return }
+        window = UIWindow(windowScene: windowScene)
+    }
+}

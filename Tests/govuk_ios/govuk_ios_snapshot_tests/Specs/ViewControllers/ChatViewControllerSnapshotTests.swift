5321b5c4ab9f8c292b32897fd0d906254dbd1a56
Joshua Dubey <jodubey@deloitte.co.uk>
2025-09-15 17:47:11 +0100

    Update snapshots

--- Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/ChatViewControllerSnapshotTests.swift
+++ Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/ChatViewControllerSnapshotTests.swift
@@ -115,6 +115,22 @@ final class ChatViewControllerSnapshotTests: SnapshotTestCase {
         )
     }
 
+    func test_loadInNavigationController_markdown_light_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            view: markdownView(),
+            mode: .light,
+            navBarHidden: true
+        )
+    }
+
+    func test_loadInNavigationController_markdown_dark_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            view: markdownView(),
+            mode: .dark,
+            navBarHidden: true
+        )
+    }
+
     private func view(showError: Bool = false,
                       includeSources: Bool = true) -> some View {
         let mockChatService = MockChatService()
@@ -178,5 +194,52 @@ final class ChatViewControllerSnapshotTests: SnapshotTestCase {
         let view = ChatView(viewModel: viewModel)
             .environment(\.isTesting, true)
         return view
+    }
+
+    private func markdownView() -> some View {
+        let mockChatService = MockChatService()
+        let conversationId = "conversationId"
+        let createdAt = "\(Date())"
+        mockChatService._stubbedConversationId = conversationId
+        // An authenticationError will prevent the history from loading on
+        // view appearance
+        mockChatService._stubbedQuestionResult = .failure(.authenticationError)
+
+        let viewModel = ChatViewModel(
+            chatService: mockChatService,
+            analyticsService: MockAnalyticsService(),
+            openURLAction: { _ in },
+            handleError: { _ in }
+        )
+        viewModel.askQuestion()
+        // With no history loading to create cellModels, modify the array
+        // directly
+
+        let answer = Answer(
+            createdAt: createdAt,
+            id: "12345",
+            message: """
+                # Heading 1
+                ## Heading 2
+                ### Heading 3
+                #### Heading 4
+                ##### Heading 5
+                ###### Heading 6
+                * Bullet 1
+                * Bullet 2
+                1. Number 1
+                2. Number 2
+                """,
+            sources: nil
+        )
+
+        let cellModel = ChatCellViewModel(answer: answer,
+                                          openURLAction: { _ in },
+                                          analyticsService: MockAnalyticsService())
+        cellModel.isVisible = true
+        viewModel.cellModels = [cellModel]
+
+        return ChatView(viewModel: viewModel)
+            .environment(\.isTesting, true)
     }
 }

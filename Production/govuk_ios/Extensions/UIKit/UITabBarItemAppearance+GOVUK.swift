2a77de3d1482001aa6f5414e2fcf95ebabf101d1
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2025-09-16 10:31:06 +0100

    Added transport font

--- Production/govuk_ios/Extensions/UIKit/UITabBarItemAppearance+GOVUK.swift
+++ Production/govuk_ios/Extensions/UIKit/UITabBarItemAppearance+GOVUK.swift
@@ -7,11 +7,13 @@ extension UITabBarItemAppearance {
 
         appearance.selected.iconColor = UIColor.govUK.text.link
         appearance.selected.titleTextAttributes = [
+            .font: UIFont.govUK.tabs,
             .foregroundColor: UIColor.govUK.text.link
         ]
 
         appearance.normal.iconColor = UIColor.govUK.text.secondary
         appearance.normal.titleTextAttributes = [
+            .font: UIFont.govUK.tabs,
             .foregroundColor: UIColor.govUK.text.secondary
         ]
         return appearance

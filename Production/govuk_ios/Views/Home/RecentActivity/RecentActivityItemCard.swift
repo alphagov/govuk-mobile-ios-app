8e6c82009204b6f03b9dd9b3f9ae8df3bebbbb80
raphael olumofe <raphaelo@hotmail.co.uk>
2025-09-23 13:39:36 +0100

    point to feature branch update colours

--- Production/govuk_ios/Views/Home/RecentActivity/RecentActivityItemCard.swift
+++ Production/govuk_ios/Views/Home/RecentActivity/RecentActivityItemCard.swift
@@ -24,7 +24,7 @@ struct RecentActivityItemCard: View {
                 Spacer()
             }.padding(.vertical, 8)
             if !isLastItemInList {
-                Divider().overlay(Color.cyan)
+                Divider().overlay(Color(UIColor.govUK.strokes.listDivider))
             }
         }.background(Color(uiColor: UIColor.govUK.fills.surfaceList))
     }

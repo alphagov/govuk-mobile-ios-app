71f633d4a76259c36e5fe57aa980a2b5b24b9995
Tom Bates <batesyuk@users.noreply.github.com>
2025-09-25 13:26:43 +0100

    Fix return user missing issue. (#553)

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/Services/AnalyticsServiceTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Services/AnalyticsServiceTests.swift
@@ -120,6 +120,25 @@ struct AnalyticsServiceTests {
     }
 
     @Test
+    func trackError_rejectedPermissions_doesNothing() {
+        let mockAnalyticsClient = MockAnalyticsClient()
+        let mockUserDefaultsService = MockUserDefaultsService()
+        let mockAuthenticationService = MockAuthenticationService()
+        mockAuthenticationService._stubbedIsSignedIn = true
+        let subject = AnalyticsService(
+            clients: [mockAnalyticsClient],
+            userDefaultsService: mockUserDefaultsService,
+            authenticationService: mockAuthenticationService
+        )
+        subject.setAcceptedAnalytics(accepted: false)
+
+        let error = NSError(domain: "test", code: 1)
+        subject.track(error: error)
+
+        #expect(mockAnalyticsClient._trackErrorReceivedErrors.count == 0)
+    }
+
+    @Test
     func trackEvent_signedOut_doesNothing() {
         let mockAnalyticsClient = MockAnalyticsClient()
         let mockUserDefaults = MockUserDefaultsService()
@@ -155,6 +174,29 @@ struct AnalyticsServiceTests {
             analyticsService: subject
         )
         subject.track(screen: mockViewController)
+
+        #expect(mockAnalyticsClient._trackEventReceivedEvents.count == 0)
+    }
+
+    @Test
+    @MainActor
+    func trackError_signedOut_doesNothing() {
+        let mockAnalyticsClient = MockAnalyticsClient()
+        let mockUserDefaults = MockUserDefaultsService()
+        let mockAuthenticationService = MockAuthenticationService()
+        mockAuthenticationService._stubbedIsSignedIn = true
+        let subject = AnalyticsService(
+            clients: [mockAnalyticsClient],
+            userDefaultsService: mockUserDefaults,
+            authenticationService: mockAuthenticationService
+        )
+        subject.setAcceptedAnalytics(accepted: true)
+
+        let mockViewController = MockBaseViewController(
+            analyticsService: subject
+        )
+        let error = NSError(domain: "test", code: 1)
+        subject.track(error: error)
 
         #expect(mockAnalyticsClient._trackEventReceivedEvents.count == 0)
     }

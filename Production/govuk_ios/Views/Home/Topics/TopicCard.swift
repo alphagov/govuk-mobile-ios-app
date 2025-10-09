c067dbb8384292e0b53008c3ef40fe71b3843f17
raphael olumofe <raphaelo@hotmail.co.uk>
2025-10-09 15:01:49 +0100

    topics view wip

--- Production/govuk_ios/Views/Home/Topics/TopicCard.swift
+++ Production/govuk_ios/Views/Home/Topics/TopicCard.swift
@@ -2,28 +2,22 @@ import SwiftUI
 
 struct TopicCard: View {
     let model: Topic
-    @ScaledMetric var scale: CGFloat = 1
+    let isLastInList: Bool
     var body: some View {
-        VStack(alignment: .leading) {
-            VStack(alignment: .leading) {
-                Image(uiImage: model.icon)
-                    .foregroundColor(Color(uiColor: UIColor.govUK.text.trailingIcon))
-                    .padding([.top], 2)
-                Spacer()
-                HStack(alignment: .bottom) {
-                    Text(model.title)
-                        .font(Font.govUK.bodySemibold)
-                        .foregroundColor(Color(UIColor.govUK.text.primary))
-                        .multilineTextAlignment(.leading)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    Image(systemName: "chevron.forward")
-                        .resizable()
-                        .scaledToFit()
-                        .foregroundColor(Color(uiColor: UIColor.govUK.text.trailingIcon))
-                        .frame(width: 12 * scale, height: 12 * scale)
-                        .padding([.bottom], 3)
+        VStack {
+            HStack(spacing: 8) {
+                    Image(systemName: "cross")
+                VStack {
+                    HStack {
+                        Text(model.title)
+                        Spacer()
+                        Text(Image(systemName: "chevron.right"))
+                    }
+                    if !isLastInList {
+                        Divider()
+                    }
                 }
-            }.padding()
-        }.accessibilityAddTraits(.isButton)
+            }.padding([.horizontal, .top])
+        }
     }
 }
--- Production/govuk_ios/Views/Home/Topics/TopicsView.swift
+++ Production/govuk_ios/Views/Home/Topics/TopicsView.swift
@@ -5,72 +5,43 @@ import GOVKit
 struct TopicsView: View {
     @StateObject var viewModel: TopicsWidgetViewModel
     @State var showingEditScreen: Bool = false
-    @ScaledMetric var scale: CGFloat = 1
-    private let columns = [
-        GridItem(.flexible(), spacing: nil, alignment: .leading),
-        GridItem(.flexible(), spacing: nil, alignment: .leading)
-    ]
+    @Environment(\.defaultMinListRowHeight) var minRowHeight
 
     var body: some View {
         if viewModel.fetchTopicsError {
             VStack {
-                HStack {
-                    Text(String.home.localized("topicsWidgetTitleCustomised"))
-                        .font(Font.govUK.title3Semibold)
-                    Spacer()
-                }
-                AppErrorView(
-                    viewModel: self.viewModel.topicErrorViewModel
-                )
-                Spacer()
+                Text("error")
             }.padding()
         } else {
             VStack {
                 VStack(alignment: .leading) {
-                HStack {
-                    Text(viewModel.widgetTitle)
-                        .font(Font.govUK.title3Semibold)
-                        .foregroundColor(Color(UIColor.govUK.text.primary))
-                        .padding([.leading], 4)
-                    Spacer()
-                    Button(
-                        action: {
-                            showingEditScreen.toggle()
-                        }, label: {
-                            Text(viewModel.editButtonTitle)
-                                .foregroundColor(Color(UIColor.govUK.text.link))
-                                .font(Font.govUK.subheadlineSemibold)
-                        }
-                    )
-                }
-            }.padding(.horizontal)
-                    LazyVGrid(columns: columns, alignment: .center) {
-                        ForEach(viewModel.topicsToBeDisplayed, id: \.self) { topic in
-                            TopicCard(model: topic)
-                                .background(Color(uiColor: UIColor.govUK.fills.surfaceCardBlue))
-                                .roundedBorder(
-                                    cornerRadius: 10,
-                                    borderColor: Color(uiColor: UIColor.govUK.strokes.cardBlue),
-                                    borderWidth: 1
-                                ).padding(2)
-                                .onTapGesture {
-                                    viewModel.trackECommerceSelection(topic.title)
-                                    viewModel.topicAction(topic)
-                                }
-                        }
-                    }.padding(.horizontal)
-                if !viewModel.showAllTopicsButton {
                     HStack {
+                        Text(viewModel.widgetTitle)
+                            .font(Font.govUK.title3Semibold)
+                            .foregroundColor(Color(UIColor.govUK.text.primary))
+                            .padding([.leading], 4)
                         Spacer()
-                        SwiftUIButton(
-                            .compact,
-                            viewModel: viewModel.showAllButtonViewModel
+                        Button(
+                            action: {
+                                showingEditScreen.toggle()
+                            }, label: {
+                                Text(viewModel.editButtonTitle)
+                                    .foregroundColor(Color(UIColor.govUK.text.link))
+                                    .font(Font.govUK.subheadlineSemibold)
+                            }
                         )
-                        .frame(width: 150 * scale)
-                        Spacer()
                     }
-                    .padding()
+                }.padding(.horizontal)
+                VStack(spacing: 0) {
+                ForEach(viewModel.topicsToBeDisplayed, id: \.self) { topic in
+                    TopicCard(model: topic, isLastInList: <#Bool#>, )
+                        .background(Color(uiColor: UIColor.govUK.fills.surfaceCardBlue))
+                        .onTapGesture {
+                            viewModel.trackECommerceSelection(topic.title)
+                            viewModel.topicAction(topic)
+                        }
                 }
+            }
             }.sheet(isPresented: $showingEditScreen,
                     onDismiss: {
                 viewModel.fetchTopics()

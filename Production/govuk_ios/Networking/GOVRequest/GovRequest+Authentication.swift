a3c72be46c5f43aebe1e1c0a0bbc7c149308999d
Tom Bates <batesyuk@users.noreply.github.com>
2025-10-13 09:51:20 +0100

    GOVUKAPP-2647 - Revoke token endpoint (#576)
    * Updated revokeTokenURL to use TOKEN_BASE_URL
    
    * Updated revoke request implementation
    
    ---------
    
    Co-authored-by: Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>

--- Production/govuk_ios/Networking/GOVRequest/GovRequest+Authentication.swift
+++ Production/govuk_ios/Networking/GOVRequest/GovRequest+Authentication.swift
@@ -2,13 +2,15 @@ import Foundation
 import GOVKit
 
 extension GOVRequest {
-    static func revoke(_ token: String,
+    static func revoke(token: String,
                        clientId: String) -> GOVRequest {
         GOVRequest(
             urlPath: "/oauth2/revoke",
             method: .post,
-            bodyParameters: ["token": token,
-                            "client_id": clientId],
+            bodyParameters: [
+                "token": token,
+                "client_id": clientId
+            ],
             queryParameters: nil,
             additionalHeaders: ["Content-Type": "application/x-www-form-urlencoded"]
         )

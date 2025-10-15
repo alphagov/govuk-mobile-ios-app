8adfa17c537d7d6d64dcb2beac34bfcdecc12d62
raphael olumofe <raphaelo@hotmail.co.uk>
2025-10-15 10:10:07 +0100

    got rid of old layout

--- Production/govuk_ios/Views/Home/Topics/TopicsWidget.swift
+++ Production/govuk_ios/Views/Home/Topics/TopicsWidget.swift
@@ -4,12 +4,6 @@ import GOVKit
 
 struct TopicsWidget: View {
     @StateObject var viewModel: TopicsWidgetViewModel
-    @State var showingEditScreen: Bool = false
-    @ScaledMetric var scale: CGFloat = 1
-    private let columns = [
-        GridItem(.flexible(), spacing: nil, alignment: .leading),
-        GridItem(.flexible(), spacing: nil, alignment: .leading)
-    ]
 
     var body: some View {
         if viewModel.fetchTopicsError {
@@ -36,7 +30,7 @@ struct TopicsWidget: View {
                         Spacer()
                         Button(
                             action: {
-                                showingEditScreen.toggle()
+                                
                             }, label: {
                                 Text(viewModel.editButtonTitle)
                                     .foregroundColor(
@@ -46,54 +40,9 @@ struct TopicsWidget: View {
                             }
                         )
                     }
-                }
-                LazyVGrid(columns: columns, alignment: .center) {
-                    ForEach(viewModel.topicsToBeDisplayed, id: \.self) { topic in
-                        TopicCard(model: topic)
-                            .background(Color(uiColor: UIColor.govUK.fills.surfaceCardBlue))
-                            .roundedBorder(
-                                cornerRadius: 10,
-                                borderColor: Color(uiColor: UIColor.govUK.strokes.cardBlue),
-                                borderWidth: 1
-                            )
-                            .onTapGesture {
-                                viewModel.trackECommerceSelection(topic.title)
-                                viewModel.topicAction(topic)
-                            }
+                    VStack {
+
                     }
-                }
-                if !viewModel.showAllTopicsButton {
-                    HStack {
-                        Spacer()
-                        SwiftUIButton(
-                            .compact,
-                            viewModel: viewModel.showAllButtonViewModel
-                        )
-                        .frame(width: 150 * scale)
-                        Spacer()
-                    }
-                    .padding(.top)
-                }
-            }
-            .padding(.horizontal)
-            .sheet(isPresented: $showingEditScreen,
-                    onDismiss: {
-                viewModel.fetchTopics()
-                viewModel.fetchDisplayedTopics()
-                viewModel.updateShowAllButtonVisibility()
-                viewModel.trackECommerce()
-            }, content: {
-                EditTopicsView(
-                    viewModel: viewModel.editTopicViewModel
-                )
-            })
-            .onAppear {
-                viewModel.fetchTopics()
-                viewModel.fetchDisplayedTopics()
-                viewModel.updateShowAllButtonVisibility()
-                if !self.viewModel.initialLoadComplete {
-                    self.viewModel.initialLoadComplete = true
-                    self.viewModel.trackECommerce()
                 }
             }
         }

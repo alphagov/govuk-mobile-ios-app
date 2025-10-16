39bfaefa1a3e75101b091cc933d226139c07e06f
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2025-10-16 09:50:31 +0100

    [GOVUKAPP-2611] Edit topics design updates (#587)
    * Update title to large
    
    Also matches toolbar text to design weight
    
    * Update nav bar
    
    - updates nav bar item colour
    - applies background colour to nav bar for consistency with view
      background, updates view when transitioning between light / dark mode
    to correctly set colour.
    
    * Add topic selected icon
    
    * Update topic edit with initial design pass
    
    * Refactor and create files
    
    - Removes undoChanges as not needed
    - Rename and moves TopicSelectionCardView & TopicSelectionCardViewModel to
    individual files.
    
    * Add accessibility info to card
    
    * Update to correct colours
    
    * Refactor TopicSelectionCardViewModel
    
    Also adds tests
    
    * Udpates analytics + tests EditTopicsViewModel
    
    Updates analytics event to a button event vs toggle following new UX.
    
    * Adds EditTopicsView snapshot test
    
    Previous test missing, possibly due to using `NavigationView` as
    snapshot nav bar overlaps content in snapshot tests, moves
    NavigationView out of EditTopicsView which allows UIKit to handle nav
    bar in snapshot tests.
    
    Probably want to consider whether we want to handle the nav bar
    generally using SwiftUI approach (NavigationView).
    
    * Fix sonar issues

---
+++ Production/govuk_ios/Views/Topics/TopicSelectionCardView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+struct TopicSelectionCardView: View {
+    @ObservedObject var viewModel: TopicSelectionCardViewModel
+
+    var body: some View {
+        HStack(spacing: 16) {
+            ZStack {
+                Image(viewModel.iconName)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40, height: 40)
+            }
+            Text(viewModel.title)
+                .font(.govUK.bodySemibold)
+                .foregroundStyle(Color(viewModel.titleColor))
+            Spacer()
+        }
+        .padding(16)
+        .background(Color(viewModel.backgroundColor))
+        .roundedBorder(borderColor: .clear)
+        .onTapGesture {
+            viewModel.isOn.toggle()
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel(viewModel.title)
+        .accessibilityAddTraits(.isButton)
+        .accessibilityHint(viewModel.accessibilityHint)
+    }
+}

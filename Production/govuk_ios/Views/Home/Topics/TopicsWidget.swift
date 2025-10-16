82064ea29eb431fb4517402a13c78e07f48c22a4
raphael olumofe <raphaelo@hotmail.co.uk>
2025-10-16 15:27:58 +0100

    added component

--- Production/govuk_ios/Views/Home/Topics/TopicsWidget.swift
+++ Production/govuk_ios/Views/Home/Topics/TopicsWidget.swift
@@ -6,45 +6,64 @@ struct TopicsWidget: View {
     @StateObject var viewModel: TopicsWidgetViewModel
 
     var body: some View {
-        if viewModel.fetchTopicsError {
-            VStack {
-                HStack {
-                    Text(String.home.localized("topicsWidgetTitleCustomised"))
-                        .font(Font.govUK.title3Semibold)
+        VStack {
+            if viewModel.fetchTopicsError {
+                VStack {
+                    HStack {
+                        Text(String.home.localized("topicsWidgetTitleCustomised"))
+                            .font(Font.govUK.title3Semibold)
+                        Spacer()
+                    }
+                    AppErrorView(
+                        viewModel: self.viewModel.topicErrorViewModel
+                    )
                     Spacer()
                 }
-                AppErrorView(
-                    viewModel: self.viewModel.topicErrorViewModel
-                )
-                Spacer()
-            }
-            .padding(.horizontal)
-        } else {
-            VStack {
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text(viewModel.widgetTitle)
-                            .font(Font.govUK.title3Semibold)
-                            .foregroundColor(Color(UIColor.govUK.text.primary))
-                            .padding([.leading], 4)
-                        Spacer()
-                        Button(
-                            action: {
-                                
-                            }, label: {
-                                Text(viewModel.editButtonTitle)
-                                    .foregroundColor(
-                                        Color(UIColor.govUK.text.buttonSecondary)
-                                    )
-                                    .font(Font.govUK.subheadlineSemibold)
+                .padding(.horizontal)
+            } else {
+                VStack {
+                    Picker(, selection: $favoriteColor) {
+                        Text("Red").tag(0)
+                        Text("Green").tag(1)
+                        Text("Blue").tag(2)
+                                            }
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text(viewModel.widgetTitle)
+                                    .font(Font.govUK.title3Semibold)
+                                    .foregroundColor(Color(UIColor.govUK.text.primary))
+                                Spacer()
+                                Button(
+                                    action: {
+                                    }, label: {
+                                        Text(viewModel.editButtonTitle)
+                                            .foregroundColor(
+                                                Color(UIColor.govUK.text.buttonSecondary)
+                                            )
+                                            .font(Font.govUK.subheadlineSemibold)
+                                    }
+                                )
                             }
-                        )
+                            VStack {
+                                ForEach(viewModel.topicsToBeDisplayed, id: \.self) { topic in
+                                    TopicListItemView(
+                                        viewModel: .init(
+                                            title: topic.title,
+                                            tapAction: { viewModel.topicAction(topic) },
+                                            iconName: topic.iconName
+                                        )
+                                    )
+                                }
+                            }
+                        }
                     }
-                    VStack {
-
-                    }
+                    .padding([.horizontal, .top])
                 }
             }
+                .onAppear {
+                    viewModel.fetchTopics()
+                    viewModel.fetchDisplayedTopics()
+                }
         }
     }
 }

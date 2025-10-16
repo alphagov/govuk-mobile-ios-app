569ae0a596c51e7c73a8b3478b96cecbd9546921
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2025-10-16 11:20:17 +0100

    Added error code unit tests

--- Production/govuk_ios/Services/ReturningUserService.swift
+++ Production/govuk_ios/Services/ReturningUserService.swift
@@ -88,22 +88,3 @@ class ReturningUserService: ReturningUserServiceInterface {
         }
     }
 }
-
-enum ReturningUserServiceError: Error {
-    case missingIdentifierError
-    case coreDataDeletionError
-    case saveIdentifierError
-}
-
-extension ReturningUserServiceError {
-    var govukErrorCode: String {
-        switch self {
-        case .missingIdentifierError:
-            return "3.0.1"
-        case .coreDataDeletionError:
-            return "3.0.2"
-        case .saveIdentifierError:
-            return "3.0.3"
-        }
-    }
-}

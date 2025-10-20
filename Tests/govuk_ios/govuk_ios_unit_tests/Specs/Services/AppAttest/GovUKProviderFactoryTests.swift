c578cf089787013ca6045f123af2cf22359d1244
Tom Bates <thomas.bates@digital.cabinet-office@gov.uk>
2025-10-20 16:01:51 +0100

    Added EmptyTokenProviderTests

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/Services/AppAttest/GovUKProviderFactoryTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/Services/AppAttest/GovUKProviderFactoryTests.swift
@@ -10,6 +10,22 @@ import FirebaseAppCheck
 //    func createProvider_withMockApp_returnsExpectedResult() throws {
 //        let sut = GovUKProviderFactory()
 //        let firebaseApp: FirebaseAppInterface = MockFirebaseApp()
+//
 //        #expect(sut.createProvider(with: firebaseApp) == nil)
 //    }
 //}
+
+@Suite
+struct EmptyTokenProviderTests {
+
+    @Test
+    func getToken_returnsEmptyToken() async {
+        let sut = EmptyTokenProvider()
+        let token = await withCheckedContinuation { continuation in
+            sut.getToken { token, _ in
+                continuation.resume(returning: token)
+            }
+        }
+        #expect(token?.token == "")
+    }
+}

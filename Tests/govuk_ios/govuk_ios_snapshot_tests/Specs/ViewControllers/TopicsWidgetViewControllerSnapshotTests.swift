814efd46eadd6610984f7405f26edb6801e46275
raphael olumofe <raphaelo@hotmail.co.uk>
2025-10-22 09:50:02 +0100

    add snapshotTests

---
+++ Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/TopicsWidgetViewControllerSnapshotTests.swift
@@ -0,0 +1,45 @@
+import Foundation
+import XCTest
+import GOVKit
+import UIKit
+
+@testable import GOVKitTestUtilities
+@testable import govuk_ios
+
+@MainActor
+final class TopicWidgetViewControllerSnapshotTests: SnapshotTestCase {
+    let coreData = CoreDataRepository.arrangeAndLoad
+
+    func test_loadInNavigationController_light_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            viewController: viewController(),
+            mode: .light,
+            prefersLargeTitles: true
+        )
+    }
+
+    func test_loadInNavigationController_dark_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            viewController: viewController(),
+            mode: .dark,
+            prefersLargeTitles: true
+        )
+    }
+
+    private func viewController() -> UIViewController {
+        let topic = Topic(context: coreData.viewContext)
+        topic.title = "Benefits"
+        topic.ref = "benefits"
+        topic.topicDescription = "test description"
+        let viewModel = TopicsWidgetViewModel(
+            topicsService: MockTopicsService(), analyticsService: MockAnalyticsService(),
+            topicAction: { _ in }
+        )
+        viewModel.topicsToBeDisplayed = [topic]
+        let view = TopicsWidget(
+            viewModel: viewModel
+        )
+        return HostingViewController(rootView: view)
+    }
+}
+

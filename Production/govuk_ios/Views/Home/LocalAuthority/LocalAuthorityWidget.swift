ff72c308877c294d2f2f62419d869546982fe68f
raphaelolu <47435245+raphaelolu@users.noreply.github.com>
2025-10-23 10:27:52 +0100

    local - accessibility fix (#612)
    * moved title out of button
    
    * fixed indenting

--- Production/govuk_ios/Views/Home/LocalAuthority/LocalAuthorityWidget.swift
+++ Production/govuk_ios/Views/Home/LocalAuthority/LocalAuthorityWidget.swift
@@ -10,51 +10,54 @@ struct LocalAuthorityWidget: View {
     }
 
     var body: some View {
-        Button {
-            viewModel.tapAction()
-        } label: {
-            VStack {
-                HStack {
-                    Text(viewModel.title)
-                        .font(Font.govUK.title3Semibold)
-                        .foregroundColor(Color(UIColor.govUK.text.primary))
-                    Spacer()
-                }
-                .padding(.bottom, 8)
-                HStack(alignment: .center) {
-                    Spacer()
-                    VStack(alignment: .center) {
-                        Spacer()
-                        Image(systemName: "plus.circle")
-                            .foregroundColor(
-                                Color(
-                                    UIColor.govUK.text.iconTertiary
-                                )
-                            )
-                            .padding(.bottom, 6)
-                            .font(.title)
-                        Text(viewModel.description)
-                            .multilineTextAlignment(.center)
-                            .font(Font.govUK.body)
-                            .foregroundColor(
-                                Color(UIColor.govUK.text.primary))
-                            .padding(.horizontal)
-                        Spacer()
-                    }
-                    .padding()
-                    Spacer()
-                }
-                .background {
-                    Color(uiColor: UIColor.govUK.fills.surfaceList)
-                }
-                .roundedBorder(borderColor: .clear)
-                .shadow(
-                    color: Color(
-                        uiColor: UIColor.govUK.strokes.cardDefault
-                    ), radius: 0, x: 0, y: 3
-                )
+        VStack {
+            HStack {
+                Text(viewModel.title)
+                    .font(Font.govUK.title3Semibold)
+                    .foregroundColor(Color(UIColor.govUK.text.primary))
+                Spacer()
             }
             .padding(.horizontal)
+            .padding(.bottom, 8)
+            Button {
+                viewModel.tapAction()
+            } label: {
+                VStack {
+                    HStack(alignment: .center) {
+                        Spacer()
+                        VStack(alignment: .center) {
+                            Spacer()
+                            Image(systemName: "plus.circle")
+                                .foregroundColor(
+                                    Color(
+                                        UIColor.govUK.text.iconTertiary
+                                    )
+                                )
+                                .padding(.bottom, 6)
+                                .font(.title)
+                            Text(viewModel.description)
+                                .multilineTextAlignment(.center)
+                                .font(Font.govUK.body)
+                                .foregroundColor(
+                                    Color(UIColor.govUK.text.primary))
+                                .padding(.horizontal)
+                            Spacer()
+                        }
+                        .padding()
+                        Spacer()
+                    }
+                    .background {
+                        Color(uiColor: UIColor.govUK.fills.surfaceList)
+                    }
+                    .roundedBorder(borderColor: .clear)
+                    .shadow(
+                        color: Color(
+                            uiColor: UIColor.govUK.strokes.cardDefault
+                        ), radius: 0, x: 0, y: 3
+                    )
+                }
+                .padding(.horizontal)
+            }
         }
     }
 }

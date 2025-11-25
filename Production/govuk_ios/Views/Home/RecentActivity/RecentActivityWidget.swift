45a48cd309ec53e404d475317f7d29bb14228ce8
raphael olumofe <raphaelo@hotmail.co.uk>
2025-11-25 12:03:58 +0000

    retook snapshot tests

--- Production/govuk_ios/Views/Home/RecentActivity/RecentActivityWidget.swift
+++ Production/govuk_ios/Views/Home/RecentActivity/RecentActivityWidget.swift
@@ -5,45 +5,23 @@ struct RecentActivityWidget: View {
     @ObservedObject var viewModel: RecentActivityHomepageWidgetViewModel
     var body: some View {
         if viewModel.sections.isEmpty {
-            VStack(alignment: .leading) {
-                titleView
-                    .padding(.top, 16)
-                    .padding(.horizontal)
-                NonTappableCardView(
-                    text: viewModel.emptyActivityStateTitle
-                )
-                .padding(.horizontal)
-                .padding(.top, 4)
-            }
-            .padding(.top)
+            emptyStateView
         } else {
             VStack {
                 HStack {
                     titleView
                     Spacer()
-                    Button(
-                        action: {
-                            viewModel.seeAllAction()
-                        }, label: {
-                            Text(viewModel.seeAllButtonTitle)
-                                .foregroundColor(
-                                    Color(UIColor.govUK.text.buttonSecondary)
-                                )
-                                .font(Font.govUK.subheadlineSemibold)
-                        }
-                    )
+                    seeAllButton
                 }
                 .padding(.top, 16)
             }
-            .padding(.top)
-            .padding(.horizontal)
-            VStack {
-                GroupedList(
-                    content: viewModel.sections,
-                    backgroundColor: UIColor.govUK.fills.surfaceBackground
-                )
-                .padding(.top, -10)
-            }
+            .padding(.top, 16)
+            .padding(.horizontal, 16)
+            GroupedList(
+                content: viewModel.sections,
+                backgroundColor: UIColor.govUK.fills.surfaceBackground
+            )
+            .padding(.top, -10)
         }
     }
 
@@ -51,5 +29,33 @@ struct RecentActivityWidget: View {
         Text(viewModel.title)
             .font(Font.govUK.title3Semibold)
             .foregroundColor(Color(UIColor.govUK.text.primary))
+    }
+
+    private var emptyStateView: some View {
+        VStack(alignment: .leading) {
+            titleView
+                .padding(.top, 16)
+                .padding(.horizontal, 16)
+            NonTappableCardView(
+                text: viewModel.emptyActivityStateTitle
+            )
+            .padding(.horizontal, 16)
+            .padding(.top, 4)
+        }
+        .padding(.top, 16)
+    }
+
+    private var seeAllButton: some View {
+        Button(
+            action: {
+                viewModel.seeAllAction()
+            }, label: {
+                Text(viewModel.seeAllButtonTitle)
+                    .foregroundColor(
+                        Color(UIColor.govUK.text.buttonSecondary)
+                    )
+                    .font(Font.govUK.subheadlineSemibold)
+            }
+        )
     }
 }

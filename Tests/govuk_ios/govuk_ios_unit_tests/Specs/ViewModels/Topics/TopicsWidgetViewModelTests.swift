e2297488848235e698a03cbd2a0ca85a3955b707
raphael olumofe <raphaelo@hotmail.co.uk>
2025-12-18 20:50:10 +0000

    fixed unit tests

--- Tests/govuk_ios/govuk_ios_unit_tests/Specs/ViewModels/Topics/TopicsWidgetViewModelTests.swift
+++ Tests/govuk_ios/govuk_ios_unit_tests/Specs/ViewModels/Topics/TopicsWidgetViewModelTests.swift
@@ -222,7 +222,6 @@ struct TopicsWidgetViewModelTests {
         sut.refreshTopics()
         sut.initialLoadComplete = true
         sut.setTopic(topic: .all)
-        print(mockAnalyticsService._trackedEvents)
         #expect(mockAnalyticsService._trackedEvents.count == 1)
         #expect(mockAnalyticsService._trackedEvents.first?.name == "view_item_list")
         let eventParams = try #require(mockAnalyticsService._trackedEvents.first?.params)
@@ -231,37 +230,36 @@ struct TopicsWidgetViewModelTests {
         #expect((eventParams["results"] as? Int) == 2)
     }
 
-      //Commented out untill there is a better way to test this
-//    @Test
-//    @MainActor
-//    func setTopics_favouriteTopics_createsExpectedEcommerceEvent() throws {
-//        let favoriteOne = Topic.arrange(
-//            context: coreData.backgroundContext,
-//            isFavourite: true
-//        )
-//        let favoriteTwo = Topic.arrange(
-//            context: coreData.backgroundContext,
-//            isFavourite: true
-//        )
-//
-//        mockTopicService._stubbedFetchFavouriteTopics = [favoriteOne, favoriteTwo]
-//
-//        let sut = TopicsWidgetViewModel(
-//            topicsService: mockTopicService,
-//            analyticsService: mockAnalyticsService,
-//            topicAction: { _ in },
-//            dismissEditAction: { }
-//        )
-//        sut.initialLoadComplete = true
-//        sut.refreshTopics()
-//        sut.setTopic(topic: .all)
-//        sut.setTopic(topic: .favorite)
-//        #expect(mockAnalyticsService._trackedEvents.count == 1)
-//        #expect(mockAnalyticsService._trackedEvents.first?.name == "view_item_list")
-//        let eventParams = try #require(mockAnalyticsService._trackedEvents.first?.params)
-//        #expect((eventParams["item_list_name"] as? String) == "Your topics")
-//        #expect((eventParams["results"] as? Int) == 2)
-//    }
+    @Test
+    @MainActor
+    func setTopics_favouriteTopics_createsExpectedEcommerceEvent() throws {
+        let favoriteOne = Topic.arrange(
+            context: coreData.backgroundContext,
+            isFavourite: true
+        )
+        let favoriteTwo = Topic.arrange(
+            context: coreData.backgroundContext,
+            isFavourite: true
+        )
+
+        mockTopicService._stubbedFetchFavouriteTopics = [favoriteOne, favoriteTwo]
+
+        let sut = TopicsWidgetViewModel(
+            topicsService: mockTopicService,
+            analyticsService: mockAnalyticsService,
+            topicAction: { _ in },
+            dismissEditAction: { }
+        )
+        sut.initialLoadComplete = true
+        sut.refreshTopics()
+        sut.setTopic(topic: .all)
+        sut.setTopic(topic: .favorite)
+        #expect(mockAnalyticsService._trackedEvents.count == 2)
+        #expect(mockAnalyticsService._trackedEvents.first?.name == "view_item_list")
+        let eventParams = try #require(mockAnalyticsService._trackedEvents[1].params)
+        #expect((eventParams["item_list_name"] as? String) == "Your topics")
+        #expect((eventParams["results"] as? Int) == 2)
+    }
 
     @Test
     @MainActor

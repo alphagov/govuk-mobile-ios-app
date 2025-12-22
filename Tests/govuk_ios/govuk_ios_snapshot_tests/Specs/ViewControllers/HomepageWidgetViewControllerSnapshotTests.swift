abd787546b53b27416de26fb1194596781631a73
raphael olumofe <raphaelo@hotmail.co.uk>
2025-12-22 17:17:32 +0000

    update snapshot tests

--- Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/HomepageWidgetViewControllerSnapshotTests.swift
+++ Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/HomepageWidgetViewControllerSnapshotTests.swift
@@ -8,59 +8,34 @@ import UIKit
 @MainActor
 final class HomepageWidgetViewControllerSnapshotTests: SnapshotTestCase {
 
-    func test_loadInNavigationController_favouriteTopicsTabSelected_light_rendersCorrectly() {
+    func test_loadInNavigationController_light_rendersCorrectly() {
         VerifySnapshotInNavigationController(
-            viewController: viewController(topicScreen: .favorite),
+            viewController: viewController(),
             mode: .light,
             prefersLargeTitles: true
         )
     }
 
-    func test_loadInNavigationController_favouriteTopicsTabSelected_dark_rendersCorrectly() {
+    func test_loadInNavigationController_dark_rendersCorrectly() {
         VerifySnapshotInNavigationController(
-            viewController: viewController(
-                topicScreen: .favorite
-            ),
+            viewController: viewController(),
             mode: .dark,
             prefersLargeTitles: true
         )
     }
 
-    func test_loadInNavigationController_AllTopicsTabSelected_light_rendersCorrectly() {
-        VerifySnapshotInNavigationController(
-            viewController: viewController(
-                topicScreen: .all
-            ),
-            mode: .light,
-            prefersLargeTitles: true
-        )
-    }
-
-    func test_loadInNavigationController_AllTopicsTabSelected_dark_rendersCorrectly() {
-        VerifySnapshotInNavigationController(
-            viewController: viewController(
-                topicScreen: .all
-            ),
-            mode: .dark,
-            prefersLargeTitles: true
-        )
-    }
-
-    private func viewController(topicScreen: TopicSegment) -> UIViewController {
+    private func viewController() -> UIViewController {
         let viewModel = TopicsWidgetViewModel(
             topicsService: MockTopicsService(),
             analyticsService: MockAnalyticsService(),
             topicAction: {_ in},
             dismissEditAction: { }
         )
-
-        viewModel.setTopic(topic: topicScreen)
         let view = HomepageWidget(
             content: TopicsWidget(
             viewModel: viewModel
             )
         ).frame(width: 400,height: 250)
-
         return HostingViewController(rootView: view)
     }
 }
--- Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/TopicsWidgetViewSnapshotTests.swift
+++ Tests/govuk_ios/govuk_ios_snapshot_tests/Specs/ViewControllers/TopicsWidgetViewSnapshotTests.swift
@@ -11,7 +11,7 @@ final class TopicWidgetViewSnapshotTests: SnapshotTestCase {
 
     func test_loadInNavigationController_populated_light_rendersCorrectly() {
         VerifySnapshotInNavigationController(
-            viewController: viewController(),
+            viewController: viewController(topicScreen: .favorite),
             mode: .light,
             prefersLargeTitles: true
         )
@@ -19,13 +19,29 @@ final class TopicWidgetViewSnapshotTests: SnapshotTestCase {
 
     func test_loadInNavigationController_populated_dark_rendersCorrectly() {
         VerifySnapshotInNavigationController(
-            viewController: viewController(),
+            viewController: viewController(topicScreen: .favorite),
             mode: .dark,
             prefersLargeTitles: true
         )
     }
 
-    private func viewController() -> UIViewController {
+    func test_loadInNavigationController_populated_allTopics_light_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            viewController: viewController(topicScreen: .all),
+            mode: .light,
+            prefersLargeTitles: true
+        )
+    }
+
+    func test_loadInNavigationController_populated_allTopics_dark_rendersCorrectly() {
+        VerifySnapshotInNavigationController(
+            viewController: viewController(topicScreen: .all),
+            mode: .dark,
+            prefersLargeTitles: true
+        )
+    }
+
+    private func viewController(topicScreen: TopicSegment) -> UIViewController {
 
         let mockTopicService = MockTopicsService()
         let favouriteOne = Topic.arrange(
@@ -35,6 +51,8 @@ final class TopicWidgetViewSnapshotTests: SnapshotTestCase {
         mockTopicService._stubbedHasCustomisedTopics = true
         let allOne = Topic.arrange(context: coreData.backgroundContext)
         let allTwo = Topic.arrange(context: coreData.backgroundContext)
+        allOne.title = "test"
+        allTwo.title = "test2"
 
         mockTopicService._stubbedFetchFavouriteTopics = [favouriteOne]
         mockTopicService._stubbedFetchAllTopics = [allOne, allTwo, favouriteOne]
@@ -45,6 +63,7 @@ final class TopicWidgetViewSnapshotTests: SnapshotTestCase {
             topicAction: { _ in },
             dismissEditAction: { }
         )
+        viewModel.setTopic(topic: topicScreen)
         let view = TopicsWidget(
             viewModel: viewModel
         )

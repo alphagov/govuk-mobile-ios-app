b2dea72c8bd3098dc2b54bb9beb5450e2e229daa
Krzysztof.Skierniewski <krzysztof.skierniewski@digital.cabinet-office.gov.uk>
2026-01-09 11:26:11 +0000

    added overscroll effect to HomeContentView

--- Production/govuk_ios/Views/Home/HomeContentView.swift
+++ Production/govuk_ios/Views/Home/HomeContentView.swift
@@ -5,30 +5,37 @@ struct HomeContentView: View {
     @StateObject var viewModel: HomeViewModel
     @Namespace var topID
 
+    private let scrollViewCoordinateSpace = "scroll"
+
     init(viewModel: HomeViewModel) {
         _viewModel = StateObject(wrappedValue: viewModel)
     }
 
     var body: some View {
-        ScrollViewReader { proxy in
-            ScrollView {
-                EmptyView()
-                    .id(topID)
-                ForEach(viewModel.widgets) { widget in
-                    widget
-                }
-            }
-            .onChange(of: viewModel.homeContentScrollToTop) { shouldScroll in
-                if shouldScroll {
-                    withAnimation {
-                        proxy.scrollTo(topID, anchor: .top)
+        GeometryReader { geometry in
+            ScrollViewReader { proxy in
+                ScrollView {
+                    EmptyView()
+                        .id(topID)
+                    ForEach(viewModel.widgets) { widget in
+                        widget
                     }
-                    viewModel.homeContentScrollToTop = false
+                    OverscrollIndicatorView(scrollViewHeight: geometry.size.height,
+                                            scrollViewCoordinateSpace: scrollViewCoordinateSpace)
                 }
+                .onChange(of: viewModel.homeContentScrollToTop) { shouldScroll in
+                    if shouldScroll {
+                        withAnimation {
+                            proxy.scrollTo(topID, anchor: .top)
+                        }
+                        viewModel.homeContentScrollToTop = false
+                    }
+                }
+                .coordinateSpace(name: scrollViewCoordinateSpace)
+            }.onAppear {
+                viewModel.updateWidgets()
             }
-        }.onAppear {
-            viewModel.updateWidgets()
+            .background(Color(uiColor: UIColor.govUK.fills.surfaceBackground))
         }
-        .background(Color(uiColor: UIColor.govUK.fills.surfaceBackground))
     }
 }
